097b43523e8432caa88a04cc94c60ba1c6449aa8
aksana.bazhenava <bazhenava1.17@gmail.com>
2024-07-24 22:08:34 +0300

    add Calculator class, add code to main method

---
+++ src/main/java/by/itacademy/aksanabazhenava/Calculator.java
@@ -0,0 +1,16 @@
+package by.itacademy.aksanabazhenava;
+
+public class Calculator {
+
+    public static int add(int a, int b) {
+        int addResult;
+        addResult = a + b;
+        return addResult;
+    }
+
+    public static int multiplay(int a, int b) {
+        int multiplaResult;
+        multiplaResult = a * b;
+        return multiplaResult;
+    }
+}
--- src/main/java/by/itacademy/aksanabazhenava/Main.java
+++ src/main/java/by/itacademy/aksanabazhenava/Main.java
@@ -2,6 +2,9 @@ package by.itacademy.aksanabazhenava;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Privet, privet!");
+        int a = 574;
+        int b = 644;
+        System.out.println(Calculator.add(a,b));
+        System.out.println(Calculator.multiplay(a,b));
     }
 }
