4653d9471a751277ea0a74fe2527b048f7a106c4
Denis-Baranov <denizzka@tut.by>
2024-07-24 22:21:01 +0300

    add Denis Baranov package add Main class add Main method add Calculator class refactor Main class

---
+++ src/main/java/by/itacademy/denisbaranov/Calculator.java
@@ -0,0 +1,15 @@
+package by.itacademy.denisbaranov;
+
+public class Calculator {
+    public static int sum(int a, int b) {
+        int c;
+        c = a + b;
+        return c;
+    }
+
+    public static int multiply(int a, int b) {
+        int c;
+        c = a * b;
+        return c;
+    }
+}
--- src/main/java/by/itacademy/denisbaranov/Main.java
+++ src/main/java/by/itacademy/denisbaranov/Main.java
@@ -2,6 +2,7 @@ package by.itacademy.denisbaranov;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(Calculator.sum(5, 6));
+        System.out.println(Calculator.multiply(5, 6));
     }
 }
