0be2536979ed9f92e155b0b972a2a6a9765c8268
Irina Novik <novikirishka7@gmail.com>
2024-07-24 22:26:29 +0300

    Add Calculator class

--- src/main/java/by/itacademy/irina/novik/Calculator.java
+++ src/main/java/by/itacademy/irina/novik/Calculator.java
@@ -7,6 +7,13 @@ public class Calculator {
 
     public static int multiply(int a, int b) {
         return (a * b);
+    }
 
+    public static int subtract(int a, int b) {
+        return (a - b);
+    }
+
+    public static int division(int a, int b) {
+        return (a / b);
     }
 }
