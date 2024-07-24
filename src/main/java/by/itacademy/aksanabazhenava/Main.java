f31c5b9f704c4032abf96449d28d3beddd24937e
aksana.bazhenava <bazhenava1.17@gmail.com>
2024-07-24 21:48:34 +0300

    add package and Main class, main method

--- src/main/java/by/itacademy/aksanabazhenava/Main.java
+++ src/main/java/by/itacademy/aksanabazhenava/Main.java
@@ -2,6 +2,6 @@ package by.itacademy.aksanabazhenava;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Privet!");
+        System.out.println("Privet, privet!");
     }
 }
