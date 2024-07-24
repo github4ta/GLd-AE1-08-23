0ebed72efd78d3fe43907a14ccd7186f5900e3cb
Artsiom.Mironchyk <artjommironchyk@gmail.com>
2024-07-24 20:10:17 +0300

    add ArtjomMironchyk

--- src/main/java/by/itacademy/TeamWork.java
+++ src/main/java/by/itacademy/TeamWork.java
@@ -5,7 +5,7 @@ public class TeamWork {
     public static void main(String[] args) {
         System.out.println("Hello from Sergei Tsarik");
         System.out.println("Hello from Pavel Yatsevich");
-        System.out.println("Hello from Mironchyk Artsiom");
+        System.out.println("Hello from Artsiom Mironchyk");
         System.out.println("Hello from Aksana Bazhenava");
         System.out.println("Hello from Andrei Unuchak");
     }
