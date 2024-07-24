e8d9accc919455381467e01396631a3ad0f6ab41
Olga.Zaitseva <olga.zaitseva.itacademy@gmail.com>
2024-07-24 20:23:35 +0300

    add Olga Zaitseva TeamWork

--- src/main/java/by/itacademy/TeamWork.java
+++ src/main/java/by/itacademy/TeamWork.java
@@ -11,5 +11,6 @@ public class TeamWork {
         System.out.println("Hello from Irina Novik");
         System.out.println("Hello from Dmitriy Medvedev");
         System.out.println("Hello from Sergey Melnik");
+        System.out.println("Hello from Olga Zaitseva!");
     }
 }
