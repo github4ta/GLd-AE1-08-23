4e0be38bec6df9f536ea0ca5b75698876355c972
serge <sergei-msg@tut.by>
2024-07-24 22:05:33 +0300

    add methods add and multiply Sergey Melnik

---
+++ src/main/java/by/itacademy/sergeymelnik/Calculator.java
@@ -0,0 +1,16 @@
+package by.itacademy.sergeymelnik;
+
+public class Calculator {
+    public static int add(int a, int b){
+        int sum = 0;
+        sum = a+b;
+
+        return sum;
+    }
+    public static int multiply(int a, int b){
+        int mult=0;
+        mult = a*b;
+        return mult;
+    }
+
+}
--- src/main/java/by/itacademy/sergeymelnik/Main.java
+++ src/main/java/by/itacademy/sergeymelnik/Main.java
@@ -1,4 +1,8 @@
 package by.itacademy.sergeymelnik;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(Calculator.add(5,5));
+        System.out.println(Calculator.multiply(10,10));
+    }
 }
