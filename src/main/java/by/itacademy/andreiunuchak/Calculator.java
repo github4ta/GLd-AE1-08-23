737365b75a655ddc8cc113225545b1baf000e7c6
Andrei Unuchak <andrei.unuchak@gmail.com>
2024-07-24 21:18:26 +0200

    add divide method to Calculator.java add subtract method to Calculator.java

--- src/main/java/by/itacademy/andreiunuchak/Calculator.java
+++ src/main/java/by/itacademy/andreiunuchak/Calculator.java
@@ -8,4 +8,12 @@ public class Calculator {
     public static int multiply(int a, int b){
         return a*b;
     }
+
+    public static double divide(int a, int b){
+        return (double) a/b;
+    }
+
+    public static int subtract(int a, int b){
+        return a-b;
+    }
 }
