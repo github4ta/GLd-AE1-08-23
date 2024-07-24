1a170aba9a7379ec1e0c082b5ac9df0b7c0bb262
Olga.Zaitseva <olga.zaitseva.itacademy@gmail.com>
2024-07-24 22:26:22 +0300

    add Method

--- src/main/java/by/itacademy/olgazaitseva/Calculator.java
+++ src/main/java/by/itacademy/olgazaitseva/Calculator.java
@@ -8,4 +8,11 @@ public class Calculator {
     public static int multiply(int a, int b) {
         return (a * b);
     }
+    public static int deduct(int a, int b) {
+
+        return (a - b);
+    }
+    public static int divide(int a, int b) {
+        return (a / b);
+    }
 }
--- src/main/java/by/itacademy/olgazaitseva/Main.java
+++ src/main/java/by/itacademy/olgazaitseva/Main.java
@@ -6,5 +6,7 @@ public class Main {
         int b = 3;
         System.out.println(Calculator.add(a, b));
         System.out.println(Calculator.multiply(a, b));
+        System.out.println(Calculator.deduct(a, b));
+        System.out.println(Calculator.divide(a, b));
     }
 }
