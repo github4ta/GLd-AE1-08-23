e6c18476b9fc100489de43e01d5a164507df71fb
aksana.bazhenava <bazhenava1.17@gmail.com>
2024-07-24 11:07:09 +0300

    1) add Aksana Bazhenava 2) add Aksana Bazhenava package 3) add Aksana Bazhenava classes

---
+++ src/main/java/by/itacademy/AksanaBazhenava.java
@@ -0,0 +1,8 @@
+package by.itacademy;
+
+public class AksanaBazhenava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Aksana!");
+    }
+}
---
+++ src/main/java/by/itacademy/aksanabazhenava/MainAksanaBazhenava.java
@@ -0,0 +1,8 @@
+package by.itacademy.aksanabazhenava;
+
+public class MainAksanaBazhenava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Aksana!");
+    }
+}
