16f82ba59190a06fd3084298e61e9ad2ec37a785
Olga.Zaitseva <olga.zaitseva.itacademy@gmail.com>
2024-07-24 22:15:11 +0300

    add Main

---
+++ src/main/java/by/itacademy/olgazaitseva/Main.java
@@ -0,0 +1,10 @@
+package by.itacademy.olgazaitseva;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        System.out.println(Calculator.add(a, b));
+        System.out.println(Calculator.multiply(a, b));
+    }
+}
