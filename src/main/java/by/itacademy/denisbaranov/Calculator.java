f3af854ef7e03986e87ecd1c4e8bede3ea1a451b
Denis-Baranov <denizzka@tut.by>
2024-07-24 22:29:08 +0300

    refactor Calculator class refactor Main class

--- src/main/java/by/itacademy/denisbaranov/Calculator.java
+++ src/main/java/by/itacademy/denisbaranov/Calculator.java
@@ -2,14 +2,18 @@ package by.itacademy.denisbaranov;
 
 public class Calculator {
     public static int sum(int a, int b) {
-        int c;
-        c = a + b;
-        return c;
+        return (a + b);
+    }
+
+    public static int subtract(int a, int b) {
+        return (a - b);
     }
 
     public static int multiply(int a, int b) {
-        int c;
-        c = a * b;
-        return c;
+        return (a * b);
+    }
+
+    public static double divide(double a, double b) {
+        return (a / b);
     }
 }
