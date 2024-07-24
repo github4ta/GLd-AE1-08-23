785d027811e23397213f602c4d006d4cebfe6d2d
Dzmitryi Miadzvedzeu <dzmitryi.miadzvedzeu@gmail.com>
2024-07-24 22:19:25 +0300

    add method subtract and method divide to class Calculator

--- src/main/java/by/itacademy/dmitriymedvedev/Calculator.java
+++ src/main/java/by/itacademy/dmitriymedvedev/Calculator.java
@@ -4,7 +4,16 @@ public class Calculator {
     public static int add(int a, int b) {
         return a + b;
     }
+
     public static int multiply(int a, int b) {
         return a * b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
     }
 }
--- src/main/java/by/itacademy/dmitriymedvedev/Main.java
+++ src/main/java/by/itacademy/dmitriymedvedev/Main.java
@@ -1,4 +1,10 @@
 package by.itacademy.dmitriymedvedev;
 
 public class Main {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        System.out.println(Calculator.divide(a,b));
+
+    }
 }
