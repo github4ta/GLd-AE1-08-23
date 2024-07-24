35f3015adaf4554007cee7edc3a16881e9528bf0
serge <sergei-msg@tut.by>
2024-07-24 20:43:56 +0300

    add upd calculator Sergey Melnik

--- src/main/java/by/itacademy/SergeyMelnikCalculator.java
+++ src/main/java/by/itacademy/SergeyMelnikCalculator.java
@@ -1,4 +1,11 @@
 package by.itacademy;
 
 public class SergeyMelnikCalculator {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        int c = a + b;
+        System.out.println(c);
+    }
 }
