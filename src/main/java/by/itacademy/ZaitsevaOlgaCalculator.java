696f7cb3b2ded389afc6895b427abc7c69ebd061
Olga.Zaitseva <olga.zaitseva.itacademy@gmail.com>
2024-07-24 20:34:09 +0300

    add Olga Zaitseva ZaitsevaOlgaCalculator

---
+++ src/main/java/by/itacademy/ZaitsevaOlgaCalculator.java
@@ -0,0 +1,10 @@
+package by.itacademy;
+
+public class ZaitsevaOlgaCalculator {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
