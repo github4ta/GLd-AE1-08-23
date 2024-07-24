89aa0f6008e28f59ea9584bcf0cd4bf515d1ac7b
Dzmitryi Miadzvedzeu <dzmitryi.miadzvedzeu@gmail.com>
2024-07-24 22:15:47 +0300

    add method add and method multiply to class Calculator

---
+++ src/main/java/by/itacademy/dmitriymedvedev/Calculator.java
@@ -0,0 +1,10 @@
+package by.itacademy.dmitriymedvedev;
+
+public class Calculator {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
