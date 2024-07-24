8801ae3e4898c1406e9bd9b893244d24e6a47093
aksana.bazhenava <bazhenava1.17@gmail.com>
2024-07-24 20:04:30 +0300

    add code

--- src/main/java/by/itacademy/TeamWork.java
+++ src/main/java/by/itacademy/TeamWork.java
@@ -5,5 +5,6 @@ public class TeamWork {
     public static void main(String[] args) {
         System.out.println("Hello from Sergei Tsarik");
         System.out.println("Hello from Mironchyk Artsiom");
+        System.out.println("Hello from Aksana Bazhenava");
     }
 }
