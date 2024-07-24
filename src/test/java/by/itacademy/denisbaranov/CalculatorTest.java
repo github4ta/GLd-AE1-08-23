8e6b0094ddf0f828633c6b7827abe268a38b8df5
Denis-Baranov <denizzka@tut.by>
2024-07-24 23:03:21 +0300

    add CalculatorTest class add pom.xml

---
+++ src/test/java/by/itacademy/denisbaranov/CalculatorTest.java
@@ -0,0 +1,17 @@
+package by.itacademy.denisbaranov;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    @Test
+    public void test1() {
+        //1.Подготовка
+        int a = 4;
+        int b = 5;
+        //2.Действие
+        int result = Calculator.sum(a, b);
+        //3. Результат
+        Assertions.assertEquals(9, result);
+    }
+}
