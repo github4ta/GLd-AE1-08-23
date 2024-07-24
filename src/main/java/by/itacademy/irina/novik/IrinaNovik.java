0b4d0384d9c4c3630055bdcc7d99231514f1e6e2
Irina Novik <novikirishka7@gmail.com>
2024-07-24 22:20:37 +0300

    Refactor Main class

--- src/main/java/by/itacademy/irina/novik/IrinaNovik.java
+++
@@ -1,7 +0,0 @@
-package by.itacademy.irina.novik;
-
-public class IrinaNovik {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/by/itacademy/irina/novik/Main.java
@@ -0,0 +1,11 @@
+package by.itacademy.irina.novik;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 8;
+        System.out.println(Calculator.add(a, b));
+        System.out.println(Calculator.multiply(a, b));
+
+    }
+}
