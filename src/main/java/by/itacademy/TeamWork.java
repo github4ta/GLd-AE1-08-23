e6e276f9b0cafbe84dc5b09006fb36ca7bfb5cb3
Dzmitryi Miadzvedzeu <dzmitryi.miadzvedzeu@gmail.com>
2024-07-24 19:59:37 +0300

    add hello from dmitriymedvedev

--- src/main/java/by/itacademy/TeamWork.java
+++ src/main/java/by/itacademy/TeamWork.java
@@ -3,6 +3,6 @@ package by.itacademy;
 public class TeamWork {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Sergei Tsarik");
+        System.out.println("Hello from Dmitriy Medvedev");
     }
 }
