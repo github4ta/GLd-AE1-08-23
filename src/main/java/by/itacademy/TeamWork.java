46dcb99443792527548424ce1d3db94560dfdef5
Andrei Unuchak <andrei.unuchak@gmail.com>
2024-07-24 19:05:56 +0200

    add Andrei Unuchak to TeamWork

--- src/main/java/by/itacademy/TeamWork.java
+++ src/main/java/by/itacademy/TeamWork.java
@@ -5,5 +5,6 @@ public class TeamWork {
     public static void main(String[] args) {
         System.out.println("Hello from Sergei Tsarik");
         System.out.println("Hello from Pavel Yatsevich");
+        System.out.println("Hello from Andrei Unuchak");
     }
 }
