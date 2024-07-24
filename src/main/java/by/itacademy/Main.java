2aeadc3cb7317a2b2fe9eff9c605cda9016593f1
Artsiom.Mironchyk <artjommironchyk@gmail.com>
2024-07-24 22:31:18 +0300

    add new class Calculator, Main

--- src/main/java/by/itacademy/Main.java
+++ src/main/java/by/itacademy/Main.java
@@ -1,7 +1,14 @@
 package by.itacademy;
 
+import by.itacademy.mironchyk.Calculator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int a = 7;
+        int b = 8;
+        System.out.println(Calculator.add(a, b));
+        System.out.println(Calculator.multiply(a, b));
+        System.out.println(Calculator.subtract(a, b));
+        System.out.println(Calculator.division(a, b));
     }
 }
--- src/main/java/by/itacademy/mironchyk/Calculator.java
+++ src/main/java/by/itacademy/mironchyk/Calculator.java
@@ -1,15 +1,19 @@
 package by.itacademy.mironchyk;
 
 public class Calculator {
-
-    public static void main(String[] args) {
-        int num1 = 20;
-        int num2 = 4;
-
-        System.out.println("Addition: " + Calculator.add(num1, num2));
+    public static int add(int a, int b) {
+        return (a + b);
     }
 
-    public static int add(int a, int b) {
-        return a + b;
+    public static int multiply(int a, int b) {
+        return (a * b);
+    }
+
+    public static int subtract(int a, int b) {
+        return (a - b);
+    }
+
+    public static int division(int a, int b) {
+        return (a / b);
     }
 }
