4b832574475bb49e45a64c505e53ed26981004ce
aksana.bazhenava <bazhenava1.17@gmail.com>
2024-07-25 14:00:49 +0300

    rename class MainGEO -> MainGeometry, refactor classes MainGeometry and Rectangle

---
+++ src/main/java/by/itacademy/aksanabazhenava/geometry/MainGeometry.java
@@ -0,0 +1,26 @@
+package by.itacademy.aksanabazhenava.geometry;
+
+public class MainGeometry {
+    public static void main(String[] args) {
+        //ОКРУЖНОСТЬ
+        Circle circle1 = new Circle();
+        double radius = 59.8;
+        System.out.println("Длинна первой окружности составляет " + Circle.circleLength(radius));
+
+        Circle circle2 = new Circle(8.6);
+        Circle circle3 = new Circle(9.2);
+        System.out.println("Радиус второй окружности = " + circle2.getRadius());
+        System.out.println("Радиус третьей окружности = " + circle3.getRadius());
+        System.out.println("Если сравнить вторую и третью окружности, то их радиусы равны (или нет): " + circle2.equals(circle3) + "\n");
+
+        //ПРЯМОУГОЛЬНИК
+        Rectangle rectangle1 = new Rectangle();
+        System.out.println("Площадь первого прямоугольника равна " + Rectangle.rectangleSquare(58, 17));
+        Rectangle rectangle2 = new Rectangle();
+        System.out.println("Площадь второго прямоугольника равна " + Rectangle.rectangleSquare(11, 25));
+        System.out.println("Если сравнить первый и второй прямоугольники, то они равны (или нет): " + rectangle1.equals(rectangle2));
+
+        Rectangle rectangle3 = new Rectangle(8, 7);
+        System.out.println("Длинна и высота третьего прямоугольника = " + rectangle3.getLength() + " и " + rectangle3.getWidth());
+    }
+}
--- src/main/java/by/itacademy/aksanabazhenava/geometry/Rectangle.java
+++ src/main/java/by/itacademy/aksanabazhenava/geometry/Rectangle.java
@@ -1,4 +1,49 @@
 package by.itacademy.aksanabazhenava.geometry;
 
 public class Rectangle {
+    int length;
+    int width;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public static int rectangleSquare(int length, int width) {
+        return length * width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
