e4b0a82f4ae37220b43208dfeedd05ecefe923ec
aksana.bazhenava <bazhenava1.17@gmail.com>
2024-07-25 16:45:35 +0300

    refactor class CalculatorTest

--- src/test/java/by/itacademy/aksanabazhenava/CalculatorTest.java
+++ src/test/java/by/itacademy/aksanabazhenava/CalculatorTest.java
@@ -1,4 +1,19 @@
 package by.itacademy.aksanabazhenava;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CalculatorTest {
+    @Test
+    public void testAdd() {
+        // 1. Подготовка
+        int a = 4;
+        int b = 6;
+
+        //2. Действия
+        int result = Calculator.add(a, b);
+
+        //3. Проверка
+        Assertions.assertEquals(10, result);
+    }
 }
