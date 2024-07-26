ae9ac635f64d060ef3f4b7bb982b833e9ec8fd67
Andrei Unuchak <andrei.unuchak@gmail.com>
2024-07-26 19:18:04 +0200

    add Faker dependency add User.java with constructor and basic methods add util Users.java with generateRandomUser() method

---
+++ src/main/java/by/itacademy/andreiunuchak/user/User.java
@@ -0,0 +1,48 @@
+package by.itacademy.andreiunuchak.user;
+
+import java.util.Objects;
+
+public class User {
+    private String login;
+    private String password;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean equals(User user) {
+        if (this == user) return true;
+        if (user == null) return false;
+        return this.login.equals(user.login) && this.password.equals(user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(login, password);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
