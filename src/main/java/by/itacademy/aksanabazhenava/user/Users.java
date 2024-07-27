a36992a1c80beec1f08e96075188bc51a83da03e
aksana.bazhenava <bazhenava1.17@gmail.com>
2024-07-27 21:40:15 +0300

    refactor Users class, pom.xml

--- src/main/java/by/itacademy/aksanabazhenava/user/Users.java
+++ src/main/java/by/itacademy/aksanabazhenava/user/Users.java
@@ -1,6 +1,12 @@
 package by.itacademy.aksanabazhenava.user;
 
+import com.github.javafaker.Faker;
+
+import java.io.UnsupportedEncodingException;
+
 public class Users {
-
-
+    public static User generateRandomUser() throws UnsupportedEncodingException {
+        Faker faker = new Faker();
+        return new User(faker.internet().userAgentAny(), faker.internet().password());
+    }
 }
