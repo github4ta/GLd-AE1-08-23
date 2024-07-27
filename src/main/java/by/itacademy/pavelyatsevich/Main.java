62b80e4826cc529776171d61633653024aa5c7f5
lu55y <mu11er100500@gmail.com>
2024-07-27 15:53:21 +0300

    refactor Main class

--- src/main/java/by/itacademy/pavelyatsevich/Main.java
+++ src/main/java/by/itacademy/pavelyatsevich/Main.java
@@ -1,20 +1,9 @@
 package by.itacademy.pavelyatsevich;
 
-import by.itacademy.pavelyatsevich.calculator.Calculator;
+import by.itacademy.pavelyatsevich.user.Users;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("to add, enter numbers");
-        System.out.println("summ of two numbers: "
-                + Calculator.add(Calculator.getNumber(), Calculator.getNumber()));
-        System.out.println("for multiplication enter numbers");
-        System.out.println("multiply of two numbers: "
-                + Calculator.multiply(Calculator.getNumber(), Calculator.getNumber()));
-        System.out.println("to subtract, enter numbers");
-        System.out.println("subtract of two numbers: "
-                + Calculator.subtract(Calculator.getNumber(), Calculator.getNumber()));
-        System.out.println("for division, enter numbers");
-        System.out.println("divide of two numbers: "
-                + Calculator.divide(Calculator.getNumber(), Calculator.getNumber()));
+        System.out.println(Users.generateRandomUser());
     }
 }
