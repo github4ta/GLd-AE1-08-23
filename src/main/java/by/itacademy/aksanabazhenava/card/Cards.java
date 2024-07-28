ad9c7826a2c901e36b8580ef7fdf0bb1878ec13d
aksana.bazhenava <bazhenava1.17@gmail.com>
2024-07-29 00:10:31 +0300

    refactor Cards class

--- src/main/java/by/itacademy/aksanabazhenava/card/Cards.java
+++ src/main/java/by/itacademy/aksanabazhenava/card/Cards.java
@@ -7,11 +7,12 @@ public class Cards {
 
     public static Card generateRandomCard() {
         String cardNumber = generateRandomNumberCard();
-        String expiredDate = generateRandomExpiredDateCard();
-        String holder = String.valueOf(generateRandomHolderCard());
+        String expiredDate = String.valueOf(generateRandomExpiredDateCard());
+        String holder = generateRandomHolderCard();
         return new Card(cardNumber, expiredDate, holder);
     }
-    public static String generateRandomNumberCard() {
+
+       public static String generateRandomNumberCard() {
         StringBuilder number = new StringBuilder();
         Random random = new Random();
         for (int i = 0; i < 16; i++) {
@@ -20,16 +21,17 @@ public class Cards {
         }
         return number.toString();
     }
-    public static LocalDate generateRandomHolderCard(){
+    public static LocalDate generateRandomExpiredDateCard(){
         Random random = new Random();
         int year = LocalDate.now().getYear() - random.nextInt(10);
+
         int month = random.nextInt(12) + 1;
-        int day = random.nextInt(28) + 1; // Просто для примера, не учитывает месяц
+        int day = random.nextInt(28) + 1;
         return LocalDate.of(year, month, day);
     }
-    public static String generateRandomExpiredDateCard() {
-        String[] firstNames = {"John", "Alice", "Michael", "Emma", "David"};
-        String[] lastNames = {"Smith", "Johnson", "Brown", "Lee", "Davis"};
+    public static String generateRandomHolderCard() {
+        String[] firstNames = {"Ivan", "Alla", "Micha", "Eva", "Daniil"};
+        String[] lastNames = {"Ivanenko", "Butenko", "Grigorenko", "Lee", "Smitt"};
         Random random = new Random();
         String firstName = firstNames[random.nextInt(firstNames.length)];
         String lastName = lastNames[random.nextInt(lastNames.length)];
