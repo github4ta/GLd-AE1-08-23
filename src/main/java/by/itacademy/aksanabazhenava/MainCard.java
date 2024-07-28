b1e3c3b9451212eca299564fa9cc0095f9c752b9
aksana.bazhenava <bazhenava1.17@gmail.com>
2024-07-28 23:54:43 +0300

    refactor Cards class - add method generateRandomCard add MainCard class

---
+++ src/main/java/by/itacademy/aksanabazhenava/MainCard.java
@@ -0,0 +1,9 @@
+package by.itacademy.aksanabazhenava;
+
+import by.itacademy.aksanabazhenava.card.Cards;
+
+public class MainCard {
+    public static void main(String[] args) {
+        System.out.println(Cards.generateRandomCard());
+    }
+}
--- src/main/java/by/itacademy/aksanabazhenava/card/Cards.java
+++ src/main/java/by/itacademy/aksanabazhenava/card/Cards.java
@@ -1,10 +1,39 @@
 package by.itacademy.aksanabazhenava.card;
 
+import java.time.LocalDate;
+import java.util.Random;
+
 public class Cards {
 
-    public static Card generateRandomCard( ) {
-
-        return null;
+    public static Card generateRandomCard() {
+        String cardNumber = generateRandomNumberCard();
+        String expiredDate = generateRandomExpiredDateCard();
+        String holder = String.valueOf(generateRandomHolderCard());
+        return new Card(cardNumber, expiredDate, holder);
+    }
+    public static String generateRandomNumberCard() {
+        StringBuilder number = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < 16; i++) {
+            int numeral = random.nextInt(10);
+            number.append(numeral);
+        }
+        return number.toString();
+    }
+    public static LocalDate generateRandomHolderCard(){
+        Random random = new Random();
+        int year = LocalDate.now().getYear() - random.nextInt(10);
+        int month = random.nextInt(12) + 1;
+        int day = random.nextInt(28) + 1; // Просто для примера, не учитывает месяц
+        return LocalDate.of(year, month, day);
+    }
+    public static String generateRandomExpiredDateCard() {
+        String[] firstNames = {"John", "Alice", "Michael", "Emma", "David"};
+        String[] lastNames = {"Smith", "Johnson", "Brown", "Lee", "Davis"};
+        Random random = new Random();
+        String firstName = firstNames[random.nextInt(firstNames.length)];
+        String lastName = lastNames[random.nextInt(lastNames.length)];
+        return firstName + " " + lastName;
     }
 
     public static boolean isValid(Card card) {
