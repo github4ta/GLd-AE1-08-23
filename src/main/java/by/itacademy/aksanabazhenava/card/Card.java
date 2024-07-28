580bcc8f2100ab0a78769ba6e8d9c159995b8610
aksana.bazhenava <bazhenava1.17@gmail.com>
2024-07-28 22:58:19 +0300

    add package card and Card, Cards classes

---
+++ src/main/java/by/itacademy/aksanabazhenava/card/Card.java
@@ -0,0 +1,8 @@
+package by.itacademy.aksanabazhenava.card;
+
+public class Card {
+
+    String number;
+    String expiredDate;
+    String holder;
+}
---
+++ src/main/java/by/itacademy/aksanabazhenava/card/Cards.java
@@ -0,0 +1,14 @@
+package by.itacademy.aksanabazhenava.card;
+
+public class Cards {
+
+    public static Card generateRandomCard( ) {
+
+        return null;
+    }
+
+    public static boolean isValid(Card card) {
+
+        return false;
+    }
+}
