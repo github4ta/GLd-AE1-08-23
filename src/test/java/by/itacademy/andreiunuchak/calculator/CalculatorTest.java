26f8758c04d9b76aff363385aa82696e8f43fa30
Andrei Unuchak <andrei.unuchak@gmail.com>
2024-07-29 22:08:36 +0200

    [ADD] add ParameterizedTest

--- src/test/java/by/itacademy/andreiunuchak/calculator/CalculatorTest.java
+++ src/test/java/by/itacademy/andreiunuchak/calculator/CalculatorTest.java
@@ -2,41 +2,64 @@ package by.itacademy.andreiunuchak.calculator;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
 
 public class CalculatorTest {
     @Test
-    public void test1(){
+    public void test1() {
         int number1 = 1;
         int number2 = 2;
         int expected = 3;
-        int actual = Calculator.add(number1,number2);
+        int actual = Calculator.add(number1, number2);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void test2(){
+    public void test2() {
         int number1 = -1;
         int number2 = -2;
         int expected = -3;
-        int actual = Calculator.add(number1,number2);
+        int actual = Calculator.add(number1, number2);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void test3(){
+    public void test3() {
         int number1 = -1;
         int number2 = 2;
         int expected = 1;
-        int actual = Calculator.add(number1,number2);
+        int actual = Calculator.add(number1, number2);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void test4(){
+    public void test4() {
         int number1 = Integer.MAX_VALUE;
         int number2 = Integer.MIN_VALUE;
         int expected = -1;
-        int actual = Calculator.add(number1,number2);
+        int actual = Calculator.add(number1, number2);
         Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("addSource")
+    public void test5(int number1, int number2, int expected) {
+        int actual = Calculator.add(number1, number2);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    public static Stream<Arguments> addSource() {
+        return Stream.of(
+                Arguments.arguments(1, 2, 3),
+                Arguments.arguments(-1, -2, -3),
+                Arguments.arguments(-1, 2, 1),
+                Arguments.arguments(1, -2, -1),
+                Arguments.arguments(Integer.MAX_VALUE, Integer.MIN_VALUE, -1)
+        );
     }
 }
