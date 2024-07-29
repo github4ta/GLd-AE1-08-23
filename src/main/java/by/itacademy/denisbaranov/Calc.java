5d518c809bb8b20cee2e740e9da55043b5d55ed9
Denis-Baranov-1996 <denizzka96@gmail.com>
2024-07-29 22:55:14 +0300

    add Calc class add CalculatorTest class add by.itacademy.denisbaranov package

---
+++ src/main/java/by/itacademy/denisbaranov/Calc.java
@@ -0,0 +1,19 @@
+package by.itacademy.denisbaranov;
+
+public class Calc {
+    public static int sum(int a, int b) {
+        return (a + b);
+    }
+
+    public static int subtract(int a, int b) {
+        return (a - b);
+    }
+
+    public static int multiply(int a, int b) {
+        return (a * b);
+    }
+
+    public static double divide(double a, double b) {
+        return (a / b);
+    }
+}
