583644e2d031dc0739df0282e3277e5f1ff8f0af
lu55y <mu11er100500@gmail.com>
2024-07-29 22:51:17 +0300

    generate CalculatorTest

---
+++ src/test/java/by/itacademy/pavelyatsevich/calculator/CalculatorTest.java
@@ -0,0 +1,12 @@
+package by.itacademy.pavelyatsevich.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+    }
+}
