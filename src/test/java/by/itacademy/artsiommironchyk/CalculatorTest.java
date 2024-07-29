cb973e73e7fce8a3a73ec538c5ba2fe014b3403d
Artsiom.Mironchyk <artjommironchyk@gmail.com>
2024-07-29 21:13:42 +0300

    новая строчка message

--- src/test/java/by/itacademy/artsiommironchyk/CalculatorTest.java
+++ src/test/java/by/itacademy/artsiommironchyk/CalculatorTest.java
@@ -8,34 +8,28 @@ public class CalculatorTest {
 
     @Test
     void testAdd() {
-        //given
         int a = 10;
         int b = 2;
-        //when
         int result = Calculator.add(a, b);
-        //then
         Assertions.assertEquals(12, result);
         Assertions.assertNotEquals(11, result);
     }
 
     @Test
     void testMultiply() {
-        //given
         int a = 10;
         int b = 2;
-        //when
         int result = Calculator.multiply(a, b);
-        //then
         Assertions.assertEquals(20, result);
         Assertions.assertNotEquals(7, result);
     }
 
     @Test
     void testSubtract() {
-        int a = 10;
+        int a =11;
         int b = 2;
         int result = Calculator.subtract(a, b);
-        Assertions.assertEquals(8, result);
+        Assertions.assertEquals(8, result,"Ваш метод работает корректно с не корректными данными");
         Assertions.assertNotEquals(9, result);
     }
 }
