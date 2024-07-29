93db7fec0eaf9b6081959bc6e5bc9dd195a35444
serge <sergei-msg@tut.by>
2024-07-29 23:17:00 +0300

    deleted all by Sergey Melnik

--- src/main/java/by/itacademy/sergeymelnik/calculator/Calculator.java
+++ src/main/java/by/itacademy/sergeymelnik/calculator/Calculator.java
@@ -1,5 +1,23 @@
 package by.itacademy.sergeymelnik.calculator;
 
 public class Calculator {
+    public static int sum(int a, int b){
+        return a+b;
+    }
+    public static double multiply(double a, double b){
+        return a*b;
+    }
+    public static int deviding(int a, int b){
+        return a/b;
+    }
+    public static double deviding(double a, double b){
+        return a/b;
+    }
+    public static int subtraction(int a, int b){
+        return a-b;
+    }
+    public static double subtraction(double a, double b){
+        return a-b;
+    }
 
 }
