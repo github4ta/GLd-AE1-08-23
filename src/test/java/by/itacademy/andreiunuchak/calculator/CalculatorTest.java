4d2cf7955726153f40f433db41e5dbfc8e174a49
Andrei Unuchak <andrei.unuchak@gmail.com>
2024-07-29 20:16:29 +0200

    [REFACTOR] Beautify of CalculatorTest

--- src/test/java/by/itacademy/andreiunuchak/calculator/CalculatorTest.java
+++ src/test/java/by/itacademy/andreiunuchak/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package by.itacademy.andreiunuchak.calculator;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -7,71 +8,68 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
-
 public class CalculatorTest {
 
     @ParameterizedTest
     @MethodSource("addDataProvider")
     void calculatorAddTest(int a, int b, int expected) {
-        assertEquals(expected, Calculator.add(a, b));
+        Assertions.assertEquals(expected, Calculator.add(a, b));
+    }
+
+    static Stream<Arguments> addDataProvider() {
+        return Stream.of(
+                Arguments.arguments(10, 15, 25),
+                Arguments.arguments(0, 0, 0),
+                Arguments.arguments(-10, -10, -20),
+                Arguments.arguments(-10, 15, 5)
+        );
     }
 
     @ParameterizedTest
     @MethodSource("multiplyDataProvider")
     void calculatorMultiplyTest(int a, int b, int expected) {
-        assertEquals(expected, Calculator.multiply(a, b));
+        Assertions.assertEquals(expected, Calculator.multiply(a, b));
+    }
+
+    static Stream<Arguments> multiplyDataProvider() {
+        return Stream.of(
+                Arguments.arguments(10, 15, 150),
+                Arguments.arguments(0, 0, 0),
+                Arguments.arguments(-10, -10, 100),
+                Arguments.arguments(-10, 15, -150)
+        );
     }
 
     @ParameterizedTest
     @MethodSource("divideDataProvider")
     void calculatorDivideTest(int a, int b, double expected) {
-        assertEquals(expected, Calculator.divide(a, b), 0.0000001);
+        Assertions.assertEquals(expected, Calculator.divide(a, b), 0.0000001);
+    }
+
+    static Stream<Arguments> divideDataProvider() {
+        return Stream.of(
+                Arguments.arguments(15, 10, 1.5),
+                Arguments.arguments(-10, -10, 1),
+                Arguments.arguments(1, 3, 0.3333333)
+        );
     }
 
     @Test
     void calculatorDivideByZeroTest() {
-        assertThrows(ArithmeticException.class, () -> Calculator.divide(0, 0));
+        Assertions.assertThrows(ArithmeticException.class, () -> Calculator.divide(0, 0));
     }
 
     @ParameterizedTest
     @MethodSource("subtractDataProvider")
     void calculatorSubtractTest(int a, int b, int expected) {
-        assertEquals(expected, Calculator.subtract(a, b));
-    }
-
-    static Stream<Arguments> addDataProvider() {
-        return Stream.of(
-                arguments(10, 15, 25),
-                arguments(0, 0, 0),
-                arguments(-10, -10, -20),
-                arguments(-10, 15, 5)
-        );
-    }
-
-    static Stream<Arguments> multiplyDataProvider() {
-        return Stream.of(
-                arguments(10, 15, 150),
-                arguments(0, 0, 0),
-                arguments(-10, -10, 100),
-                arguments(-10, 15, -150)
-        );
+        Assertions.assertEquals(expected, Calculator.subtract(a, b));
     }
 
     static Stream<Arguments> subtractDataProvider() {
         return Stream.of(
-                arguments(15, 10, 5),
-                arguments(-10, -10, 0),
-                arguments(-10, 15, -25)
-        );
-    }
-
-    static Stream<Arguments> divideDataProvider() {
-        return Stream.of(
-                arguments(15, 10, 1.5),
-                arguments(-10, -10, 1),
-                arguments(1, 3, 0.3333333)
+                Arguments.arguments(15, 10, 5),
+                Arguments.arguments(-10, -10, 0),
+                Arguments.arguments(-10, 15, -25)
         );
     }
 }
