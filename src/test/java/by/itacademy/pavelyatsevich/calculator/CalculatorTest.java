5741db20a18cd6dd8f6bc1d22f2828d838cd897b
lu55y <mu11er100500@gmail.com>
2024-07-29 20:36:02 +0300

    refactor CalculatorTest class

--- src/test/java/by/itacademy/pavelyatsevich/calculator/CalculatorTest.java
+++ src/test/java/by/itacademy/pavelyatsevich/calculator/CalculatorTest.java
@@ -6,50 +6,58 @@ import org.junit.jupiter.api.Test;
 public class CalculatorTest {
 
     @Test
-    void testAdd() {
+    public void testAdd() {
         //given
         int a = 10;
         int b = 2;
+        int validResult = 12;
+        int invalidResult = 11;
         //when
         int result = Calculator.add(a, b);
         //then
-        Assertions.assertEquals(12, result);
-        Assertions.assertNotEquals(11, result);
+        Assertions.assertEquals(validResult, result);
+        Assertions.assertNotEquals(invalidResult, result);
     }
 
     @Test
-    void testMultiply() {
+    public void testMultiply() {
         //given
         int a = 10;
         int b = 2;
+        int validResult = 20;
+        int invalidResult = 7;
         //when
         int result = Calculator.multiply(a, b);
         //then
-        Assertions.assertEquals(20, result);
-        Assertions.assertNotEquals(7, result);
+        Assertions.assertEquals(validResult, result);
+        Assertions.assertNotEquals(invalidResult, result);
     }
 
     @Test
-    void testSubtract() {
+    public void testSubtract() {
         //given
         int a = 10;
         int b = 2;
+        int validResult = 8;
+        int invalidResult = 9;
         //when
         int result = Calculator.subtract(a, b);
         //then
-        Assertions.assertEquals(8, result);
-        Assertions.assertNotEquals(9, result);
+        Assertions.assertEquals(validResult, result);
+        Assertions.assertNotEquals(invalidResult, result);
     }
 
     @Test
-    void testDivide() {
+    public void testDivide() {
         //given
         int a = 10;
         int b = 2;
+        int validResult = 5;
+        int invalidResult = 10;
         //when
         double result = Calculator.divide(a, b);
         //then
-        Assertions.assertEquals(5, result);
-        Assertions.assertNotEquals(10, result);
+        Assertions.assertEquals(validResult, result);
+        Assertions.assertNotEquals(invalidResult, result);
     }
 }
