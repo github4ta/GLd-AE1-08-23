10f4adba6f2e4ecefe7dfbf165f679b643d93dc6
aksana.bazhenava <bazhenava1.17@gmail.com>
2024-07-30 00:38:52 +0300

    refactor CalculatorTest class

--- src/test/java/by/itacademy/aksanabazhenava/CalculatorTest.java
+++ src/test/java/by/itacademy/aksanabazhenava/CalculatorTest.java
@@ -5,10 +5,34 @@ import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
     @Test
-    public void testAdd() {
-        int a = 4;
-        int b = 6;
-        int result = Calculator.add(a, b);
+    public void testAdd1() {
+        int number1 = 4;
+        int number2 = 6;
+        int result = Calculator.add(number1, number2);
         Assertions.assertEquals(10, result);
+    }
+    @Test
+    public void testAdd2(){
+        int number1 = -1;
+        int number2 = -5;
+        int expected = -6;
+        int result = Calculator.add(number1,number2);
+        Assertions.assertEquals(expected, result);
+    }
+    @Test
+    public void testAdd3(){
+        int number1 = -10;
+        int number2 = -2;
+        int expected = -12;
+        int result = Calculator.add(number1,number2);
+        Assertions.assertEquals(expected, result);
+    }
+    @Test
+    public void testAdd4() {
+        int number1 = Integer.MAX_VALUE;
+        int number2 = Integer.MIN_VALUE;
+        int expected = -1;
+        int result = Calculator.add(number1, number2);
+        Assertions.assertEquals(expected, result);
     }
 }
