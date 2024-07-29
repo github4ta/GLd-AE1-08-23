ce13ad8026f473e00bd449746d5bca7248f1fdcd
serge <sergei-msg@tut.by>
2024-07-29 22:54:45 +0300

    add double in calculator class by Sergey Melnik

---
+++ src/test/java/by/itacademy/sergey/melnik/CalculatorTest.java
@@ -0,0 +1,27 @@
+package by.itacademy.sergey.melnik;
+
+import by.itacademy.sergeymelnik.Calculator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdding(){
+        int a = 5;
+        int b = 6;
+        int calc = Calculator.adding(a, b);
+        Assertions.assertEquals(11,calc);
+    }
+
+    @Test
+    public void testMultiply(){
+        int a = 5;
+        int b = 6;
+        int calc = Calculator.multiply(a, b);
+        Assertions.assertEquals(30,calc);
+
+    }
+
+}
