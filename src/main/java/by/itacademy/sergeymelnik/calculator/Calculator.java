ccf31e2d7d6eaa4459b1e54ffb9d6c5f38a95f1b
serge <sergei-msg@tut.by>
2024-07-29 22:00:17 +0300

    add double in calculator class by Sergey Melnik

--- src/main/java/by/itacademy/sergeymelnik/calculator/Calculator.java
+++ src/main/java/by/itacademy/sergeymelnik/calculator/Calculator.java
@@ -10,7 +10,14 @@ public class Calculator {
     public static int deviding(int a, int b){
         return a/b;
     }
+    public static double deviding(double a, double b){
+        return a/b;
+    }
+
     public static int subtraction(int a, int b){
+        return a-b;
+    }
+    public static double subtraction(double a, double b){
         return a-b;
     }
 
