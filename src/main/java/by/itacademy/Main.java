9b2e7a9596d2a807637fdad081916ff25b723b02
aksana.bazhenava <bazhenava1.17@gmail.com>
2024-07-29 23:00:08 +0300

    deleted Main class refactor pom.xml

--- src/main/java/by/itacademy/Main.java
+++
@@ -1,9 +0,0 @@
-package by.itacademy;
-
-import by.itacademy.aksanabazhenava.Calculator;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(Calculator.add(5,10));
-    }
-}
