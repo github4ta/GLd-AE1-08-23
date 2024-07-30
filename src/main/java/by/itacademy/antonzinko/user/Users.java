0a0acad2ecd461b9cf7e53319d4f22650ad8c9bf
ZinkoAnton <antonzinko87@gmail.com>
2024-07-30 20:21:31 +0300

    add Anton Zinko Users

--- src/main/java/by/itacademy/antonzinko/user/Users.java
+++ src/main/java/by/itacademy/antonzinko/user/Users.java
@@ -6,7 +6,7 @@ public class Users {
     public static User generateRandomUser() {
         Faker faker = new Faker();
         String login = faker.name().firstName();
-        String password = faker.internet().password();
+        String password = faker.internet().password(1, 30, true, true, true);
         return new User(login, password);
     }
 }
