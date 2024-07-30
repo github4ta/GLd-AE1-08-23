ce6b6c7d15c3358e0c2c6822f89633d1cdae52ba
aksana.bazhenava <bazhenava1.17@gmail.com>
2024-07-30 23:27:22 +0300

    add package bazhenavaaksana.bank.card.ATM add ATM classes with constructor, getter, setter, hashcode,toString

---
+++ src/main/java/bazhenava/aksana/bank/card/atm/ATM.java
@@ -0,0 +1,53 @@
+package bazhenava.aksana.bank.card.atm;
+
+import java.util.Objects;
+
+public class ATM {
+
+    String number;
+    String address;
+    String moneyAmount;
+
+    public ATM(String number, String address, String moneyAmount) {
+        this.number = number;
+        this.address = address;
+        this.moneyAmount = moneyAmount;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMoneyAmount() {
+        return moneyAmount;
+    }
+
+    public void setMoneyAmount(String moneyAmount) {
+        this.moneyAmount = moneyAmount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ATM atm = (ATM) o;
+        return Objects.equals(number, atm.number) && Objects.equals(address, atm.address) && Objects.equals(moneyAmount, atm.moneyAmount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number, address, moneyAmount);
+    }
+}
