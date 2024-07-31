a8d2f0d20fe1ae51470ae87bc96141a69b3106b9
Olga.Zaitseva <olga.zaitseva.itacademy@gmail.com>
2024-07-31 17:12:43 +0300

    add Cicle class

---
+++ src/main/java/by/itacademy/olgazaitseva/geometry/Circle.java
@@ -0,0 +1,42 @@
+package by.itacademy.olgazaitseva.geometry;
+
+import java.util.Objects;
+
+public class Circle {
+    private double radius;
+    public Circle (double radius){
+        this.radius = radius;
+    }
+
+    public double calculateCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Circle circle = (Circle) o;
+        return Double.compare(radius, circle.radius) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(radius);
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                '}';
+    }
+}
