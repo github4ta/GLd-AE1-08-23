1160c7c9134001d43d406131d9789794f27a2348
serge <sergei-msg@tut.by>
2024-07-31 13:10:51 +0300

    added card class by Sergey Melnik

---
+++ src/main/java/by/itacademy/melniksergey/bank/card/Card.java
@@ -0,0 +1,61 @@
+package by.itacademy.melniksergey.bank.card;
+
+import java.util.Objects;
+
+public class Card {
+    String cardHolder;
+    String expiredDate;
+    String cardNumber;
+
+    public Card(String cardHolder, String expiredDate, String cardNumber) {
+        this.cardHolder = cardHolder;
+        this.expiredDate = expiredDate;
+        this.cardNumber = cardNumber;
+    }
+
+    public String getCardHolder() {
+        return cardHolder;
+    }
+
+    public void setCardHolder(String cardHolder) {
+        this.cardHolder = cardHolder;
+    }
+
+    public String getExpiredDate() {
+        return expiredDate;
+    }
+
+    public void setExpiredDate(String expiredDate) {
+        this.expiredDate = expiredDate;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Card card = (Card) o;
+        return Objects.equals(cardHolder, card.cardHolder) && Objects.equals(expiredDate, card.expiredDate) && Objects.equals(cardNumber, card.cardNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cardHolder, expiredDate, cardNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "cardHolder='" + cardHolder + '\'' +
+                ", expiredDate='" + expiredDate + '\'' +
+                ", cardNumber='" + cardNumber + '\'' +
+                '}';
+    }
+}
