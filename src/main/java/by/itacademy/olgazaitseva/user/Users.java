25e19701773ead25d0c8ec9143d3f04ef00a0a4c
Olga.Zaitseva <olga.zaitseva.itacademy@gmail.com>
2024-07-31 19:31:00 +0300

    add Users class

---
+++ src/main/java/by/itacademy/olgazaitseva/user/Users.java
@@ -0,0 +1,49 @@
+package by.itacademy.olgazaitseva.user;
+
+import java.util.Objects;
+
+public class Users {
+    private String login;
+    private String password;
+    public Users (String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Users users = (Users) o;
+        return Objects.equals(login, users.login) && Objects.equals(password, users.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(login, password);
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
