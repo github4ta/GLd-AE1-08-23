666636899701af9e5ae2dc7da04176155f17ac63
ZinkoAnton <antonzinko87@gmail.com>
2024-08-06 20:35:13 +0300

    add Anton Zinko Card

--- src/main/java/by/itacademy/antonzinko/card/Card.java
+++ src/main/java/by/itacademy/antonzinko/card/Card.java
@@ -1,17 +1,16 @@
 package by.itacademy.antonzinko.card;
 
+import java.util.Objects;
+
 public class Card {
     private String number;
-    private String expiredData;
+    private String expiredDate;
     private String holder;
 
-    public Card() {
-    }
-
-    public Card(String number, String expiredData, String holder) {
-        this.number = number;
-        this.expiredData = expiredData;
-        this.holder = holder;
+    public Card(String number, String expiredDate, String holder) {
+        setNumber(number);
+        setExpiredDate(expiredDate);
+        setHolder(holder);
     }
 
     public String getNumber() {
@@ -19,15 +18,27 @@ public class Card {
     }
 
     public void setNumber(String number) {
+        if (number == null) {
+            throw new NullPointerException();
+        }
+        if (number.length() != 16 || !isNumberCorrect(number)) {
+            throw new IllegalArgumentException("Card number is not correct");
+        }
         this.number = number;
     }
 
-    public String getExpiredData() {
-        return expiredData;
+    public String getExpiredDate() {
+        return expiredDate;
     }
 
-    public void setExpiredData(String expiredData) {
-        this.expiredData = expiredData;
+    public void setExpiredDate(String expiredDate) {
+        if (expiredDate == null) {
+            throw new NullPointerException();
+        }
+        if (!isDateFormatCorrect(expiredDate)) {
+            throw new IllegalArgumentException("Expiration date is not correct");
+        }
+        this.expiredDate = expiredDate;
     }
 
     public String getHolder() {
@@ -35,23 +46,53 @@ public class Card {
     }
 
     public void setHolder(String holder) {
+        if (holder == null) {
+            throw new NullPointerException();
+        }
+        if (holder.isEmpty() || !isHolderCorrect(holder)) {
+            throw new IllegalArgumentException("Holder name is not correct");
+        }
         this.holder = holder;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null) return false;
-        return super.equals(obj);
+    private boolean isHolderCorrect(String holder) {
+        if (holder.split(" ").length != 2) {
+            return false;
+        }
+        return holder.matches("[a-zA-Zà-ÿÀ-ß'-\\.\s]+");
+    }
+
+    private boolean isNumberCorrect(String number) {
+        return number.matches("[0-9]".repeat(16));
+    }
+
+    private boolean isDateFormatCorrect(String expiredDate) {
+        if (expiredDate.matches("[0-1][0-9]/[0-9][0-9]")) {
+            int cardMonth = Integer.parseInt(expiredDate.split("/")[0]);
+            if (cardMonth >= 1 && cardMonth <= 12) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean equals(Card card) {
+        if (this == card) return true;
+        if (card == null) return false;
+        return this.number.equals(card.number) && this.expiredDate.equals(card.expiredDate) && this.holder.equals(card.holder);
     }
 
     @Override
     public int hashCode() {
-        return super.hashCode();
+        return Objects.hash(number, expiredDate, holder);
     }
 
     @Override
     public String toString() {
-        return super.toString();
+        return "Card{" +
+                "number='" + number + '\'' +
+                ", expiredDate='" + expiredDate + '\'' +
+                ", holder='" + holder + '\'' +
+                '}';
     }
 }
