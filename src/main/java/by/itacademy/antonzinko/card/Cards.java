6a6c463c1c76188d81586aa481cdda4963d14b5a
ZinkoAnton <antonzinko87@gmail.com>
2024-08-06 20:34:43 +0300

    add Anton Zinko Cards

--- src/main/java/by/itacademy/antonzinko/card/Cards.java
+++ src/main/java/by/itacademy/antonzinko/card/Cards.java
@@ -8,10 +8,10 @@ import java.util.Random;
 public class Cards {
     public static Card generateRandomCard() {
         String number = "";
-        for(int a = 0; a < 16; a++) {
+        for (int i = 0; i < 16; i++) {
             number += String.valueOf(new Random().nextInt(10));
         }
-        int currentMonth = Calendar.getInstance().get(Calendar.YEAR);
+        int currentMonth = Calendar.getInstance().get(Calendar.MONTH) + 1;
         int generatedMonth = currentMonth + 1;
         generatedMonth = (generatedMonth > 12) ? (generatedMonth % 12) : generatedMonth;
         String monthStr = (generatedMonth < 10 ? "0" : "") + generatedMonth;
@@ -25,14 +25,14 @@ public class Cards {
     }
 
     public static boolean isValid(Card card) {
-        if(card.getExpiredData().matches("[0-1][0-9]+/[0-9][0-9]")) {
+        if (card.getExpiredDate().matches("[0-1][0-9]+/[0-9][0-9]")) {
             int currentMonth = Calendar.getInstance().get(Calendar.MONTH) + 1;
             int currentYear = Calendar.getInstance().get(Calendar.YEAR) % 100;
-            int cardMonth = Integer.parseInt(card.getExpiredData().split("/")[0]);
-            int cardYear = Integer.parseInt(card.getExpiredData().split("/")[1]);
-            if(cardYear > currentYear) {
+            int cardMonth = Integer.parseInt(card.getExpiredDate().split("/")[0]);
+            int cardYear = Integer.parseInt(card.getExpiredDate().split("/")[1]);
+            if (cardYear > currentYear) {
                 return true;
-            } else if(cardYear == currentYear && cardMonth > currentMonth) {
+            } else if (cardYear == currentYear && cardMonth > currentMonth) {
                 return true;
             }
         }
