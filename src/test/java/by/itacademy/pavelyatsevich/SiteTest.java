49be03bbc805677082ee5e0892287815b33e931f
lu55y <mu11er100500@gmail.com>
2024-08-06 19:56:36 +0300

    add field and methods to the SiteTest class

--- src/test/java/by/itacademy/pavelyatsevich/SiteTest.java
+++ src/test/java/by/itacademy/pavelyatsevich/SiteTest.java
@@ -1,4 +1,28 @@
 package by.itacademy.pavelyatsevich;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SiteTest {
+    WebDriver driver;
+
+    @BeforeEach
+    public void setupDriver() {
+        String webAddress = "https://taplink.cc/sublimasha";
+        driver = new ChromeDriver();
+        driver.get(webAddress);
+    }
+
+    @Test
+    public void openSiteTest(){
+
+    }
+
+    @AfterEach
+    public void teardown() {
+        driver.quit();
+    }
 }
