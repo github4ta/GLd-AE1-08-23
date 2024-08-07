9be231beededab654c3879ea27451375961d07bb
Aksana Bazhenava <bazhenava1.17@gmail.com>
2024-08-07 16:40:25 +0300

    refactor package bazhenava and class SiteTest refactor class SiteTest, pom.xml

--- src/main/java/by/itacademy/bazhenava/SiteTest.java
+++
@@ -1,4 +0,0 @@
-package by.itacademy.bazhenava;
-
-public class SiteTest {
-}
---
+++ src/test/java/by/itacademy/bazhenava/SiteTest.java
@@ -0,0 +1,44 @@
+package by.itacademy.bazhenava;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SiteTest {
+    static WebDriver driver;
+
+    @BeforeEach
+    public void setupDriver() {
+        String webAddress = "https://psychologists.taplink.ws";
+        driver = new ChromeDriver();
+        driver.get(webAddress);
+    }
+
+    @Test
+    public void openAndDownloadSite() {
+        //  WebDriver driver = new ChromeDriver();
+        String openAndDownloadTextXPath = "//span[contains(text(), 'Taplink.ru')]";
+        By openAndDownloadTextXPathTextBy = By.xpath(openAndDownloadTextXPath);
+        WebElement openAndDownloadTextWebElement = driver.findElement(openAndDownloadTextXPathTextBy);
+
+        String actualTextMessage = openAndDownloadTextWebElement.getText();
+        String expectedTextMessage = "Taplink.ru";
+
+        Assertions.assertEquals(expectedTextMessage, actualTextMessage);
+    }
+
+    @AfterEach
+    public void teardown() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.quit();
+    }
+}
+
+
+
+
