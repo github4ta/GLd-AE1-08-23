819796445d4af726afc77a0a9815c5e76265b838
Andrei Unuchak <andrei.unuchak@gmail.com>
2024-08-07 21:18:23 +0200

    [ADD] testButtonNotebook method

--- src/test/java/by/onliner/OnlinerTest.java
+++ src/test/java/by/onliner/OnlinerTest.java
@@ -1,9 +1,12 @@
 package by.onliner;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OnlinerTest {
@@ -20,6 +23,18 @@ public class OnlinerTest {
     @Test
     public void test() {
         // driver.findElement();
+    }
+
+    @Test
+    public void testButtonNotebook() {
+        String buttonNotebookXPath = "//a[@href='https://catalog.onliner.by/notebook']//span[@class='project-navigation__sign']";
+        By buttonNotebookBy = By.xpath(buttonNotebookXPath);
+        WebElement buttonNotebookWebElement = driver.findElement(buttonNotebookBy);
+
+        String expectedButtonText = "Ноутбуки";
+        String actualButtonText = buttonNotebookWebElement.getText();
+
+        Assertions.assertEquals(expectedButtonText, actualButtonText);
     }
 
     @AfterEach
