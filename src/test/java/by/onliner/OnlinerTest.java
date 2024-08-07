e7f498d060af187435204ea1658a02e3cf04ca18
Denis-Baranov-1996 <denizzka96@gmail.com>
2024-08-07 22:44:22 +0300

    add testAirphoneLink

--- src/test/java/by/onliner/OnlinerTest.java
+++ src/test/java/by/onliner/OnlinerTest.java
@@ -1,9 +1,12 @@
 package by.onliner;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OnlinerTest {
@@ -20,6 +23,17 @@ public class OnlinerTest {
     @Test
     public void test() {
         // driver.findElement();
+    }
+
+    @Test
+    public void testAirphoneLink() {
+        driver.manage().window().maximize();
+        String xPathAiphonesLink="//a[@href=\"https://catalog.onliner.by/headphones\"] [@class=\"project-navigation__link project-navigation__link_primary\"]";
+        By byAirphones = By.xpath(xPathAiphonesLink);
+        WebElement webElementAirphone = driver.findElement(byAirphones);
+        String actualTest = webElementAirphone.getText();
+        String expectedTest = "Наушники";
+        Assertions.assertEquals(expectedTest,actualTest);
     }
 
     @AfterEach
