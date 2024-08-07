192e68af0007c5c9fb57db736a14c9246d4bd78a
Olga.Zaitseva <olga.zaitseva.itacademy@gmail.com>
2024-08-07 22:43:54 +0300

    add OnlinerTest class

--- src/test/java/by/onliner/OnlinerTest.java
+++ src/test/java/by/onliner/OnlinerTest.java
@@ -1,9 +1,12 @@
 package by.onliner;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OnlinerTest {
@@ -19,7 +22,18 @@ public class OnlinerTest {
 
     @Test
     public void test() {
+        String videoXpath = "//a[@href=\"https://catalog.onliner.by/videocard\"]";
+        By videoXpathBy = By.xpath(videoXpath);
+        WebElement videoXpathWebElement = driver.findElement(videoXpathBy);
+
+
+
         // driver.findElement();
+
+        String actualVideoOne = videoXpathWebElement.getText();
+        String expectedVideoOne = "Видеокарты";
+        Assertions.assertEquals(expectedVideoOne, actualVideoOne);
+
     }
 
     @AfterEach
