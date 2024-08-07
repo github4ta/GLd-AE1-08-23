04532082980281d7ea7b0a1c6c8390557f036f77
serge <sergei-msg@tut.by>
2024-08-07 07:41:26 +0300

    added test to SiteTest.java by Sergey Melnik

--- src/test/java/melniksergey/SiteTest.java
+++ src/test/java/melniksergey/SiteTest.java
@@ -1,4 +1,44 @@
 package melniksergey;
 
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SiteTest {
+
+    WebDriver webDriver;
+
+
+    @BeforeEach
+    public void startBrowser(){
+
+        webDriver = new ChromeDriver();
+        webDriver.get("https://taplink.cc/psychologist");
+
+    }
+
+    @AfterEach
+    public void stopBrowser(){
+        webDriver.quit();
+    }
+
+    @Test
+    public void checkLine(){
+        String resultLinkXpath = "/html/body/div[1]/div/div/div/div[2]/div[2]/div/div/main/div/div[2]/div[2]/div/div/div/div/div/div[9]/div/div/a/div/div";
+        By resultMessageBy = By.xpath(resultLinkXpath);
+        WebElement errorMessageWebElement = webDriver.findElement(resultMessageBy);
+        String actualMessage = errorMessageWebElement.getText();
+        String expectedErroMessage = "Связаться";
+        System.out.println(actualMessage);
+
+        Assertions.assertEquals(expectedErroMessage, actualMessage);
+
+    }
+
 }
