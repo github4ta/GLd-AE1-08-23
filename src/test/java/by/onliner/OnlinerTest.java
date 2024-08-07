2da26f8095574cb985b38589815b997172d57eba
serge <sergei-msg@tut.by>
2024-08-07 22:33:35 +0300

    add test for cleaners

--- src/test/java/by/onliner/OnlinerTest.java
+++ src/test/java/by/onliner/OnlinerTest.java
@@ -1,9 +1,12 @@
 package by.onliner;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OnlinerTest {
@@ -20,6 +23,17 @@ public class OnlinerTest {
     @Test
     public void test() {
         // driver.findElement();
+    }
+
+    @Test
+    public void testCleanersLink(){
+        driver.manage().window().maximize();
+        String xPathCleanerLink = "//a[@href='https://catalog.onliner.by/vacuumcleaner']/span/span";
+        By byCleaners = By.xpath(xPathCleanerLink);
+        WebElement webElementCleaners = driver.findElement(byCleaners);
+        String actualText = webElementCleaners.getText();
+        String expetedText = "Пылесосы";
+        Assertions.assertEquals(expetedText, actualText);
     }
 
     @AfterEach
