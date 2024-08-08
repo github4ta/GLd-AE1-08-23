e11cbcc50263c821d32952b9d5000c96bf7f208d
Aksana Bazhenava <bazhenava1.17@gmail.com>
2024-08-08 21:40:02 +0300

    refactor class FooterTest, add method testHPFT002

--- src/test/java/by/onliner/homepage/FooterTest.java
+++ src/test/java/by/onliner/homepage/FooterTest.java
@@ -1,9 +1,16 @@
 package by.onliner.homepage;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class FooterTest {
 
@@ -16,10 +23,21 @@ public class FooterTest {
         driver.manage().window().maximize();
         driver.get(BASE_URL);
     }
+    @Test
+    public void testHPFT002() {
+        driver.manage().window().maximize();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(8));
+        String footerXpath = "//a[@href ='https://people.onliner.by/contacts']";
+        By footerBy = By.xpath(footerXpath);
+        WebElement footerWebElement = driver.findElement(footerBy);
 
+        String actualText = footerWebElement.getText();
+        String expectedTextMessage = "Контакты редакции";
+
+        Assertions.assertEquals(expectedTextMessage, actualText);
+    }
     @AfterEach
     public void afterEach() {
         driver.quit();
     }
-
 }
