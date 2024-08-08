4f20707e2f01b4354ddb69547e9a79f3b3eb69dc
Andrei Unuchak <andrei.unuchak@gmail.com>
2024-08-08 17:44:06 +0200

    [ADD] testRefundRules

--- src/test/java/by/onliner/homepage/FooterTest.java
+++ src/test/java/by/onliner/homepage/FooterTest.java
@@ -1,8 +1,13 @@
 package by.onliner.homepage;
 
+import by.onliner.Waiters;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FooterTest {
@@ -15,6 +20,17 @@ public class FooterTest {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(BASE_URL);
+    }
+
+    @Test
+    public void testRefundRules(){
+        String aRefundRulesXPath = "//a[@href='https://blog.onliner.by/pravila-vozvrata-tovarov-i-deneg']";
+        By aRefundRulesBy = By.xpath(aRefundRulesXPath);
+        WebElement aRefundRulesWebElement = driver.findElement(aRefundRulesBy);
+
+        String expectedText = "Правила возврата";
+        String actualText = aRefundRulesWebElement.getText();
+        Assertions.assertEquals(expectedText, actualText);
     }
 
     @AfterEach
