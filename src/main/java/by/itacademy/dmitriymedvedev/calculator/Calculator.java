f03e5de0dc05cd92b48823d735f99b34cea65369
Dzmitryi Miadzvedzeu <dzmitryi.miadzvedzeu@gmail.com>
2024-08-08 14:25:13 +0300

    refactor subtraction method in Calculator class

--- src/main/java/by/itacademy/dmitriymedvedev/calculator/Calculator.java
+++ src/main/java/by/itacademy/dmitriymedvedev/calculator/Calculator.java
@@ -9,7 +9,7 @@ public class Calculator {
         return a * b;
     }
 
-    public static int subtract(int a, int b) {
+    public static int subtraction(int a, int b) {
         return a - b;
     }
 
