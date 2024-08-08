554e9623c30db45f8a997220918668c0f567a9a6
ZinkoAnton <antonzinko87@gmail.com>
2024-08-08 11:27:36 +0300

    add Anton Zinko OnlinerTest

--- src/test/java/by/onliner/OnlinerTest.java
+++ src/test/java/by/onliner/OnlinerTest.java
@@ -26,7 +26,7 @@ public class OnlinerTest {
 
     @Test
     public void testWatchesAndBracelets() {
-        String linkWatchesAndBraceletsXPath = "//*[@id=\"container\"]/div/div/div/div/div[1]/div/div[1]/ul[2]/li[6]/a/span/span=XPath";
+        String linkWatchesAndBraceletsXPath = "//*[@id=\"container\"]/div/div/div/div/div[1]/div/div[1]/ul[2]/li[6]/a/span/span";
         By linkWatchesAndBraceletsBy = By.xpath(linkWatchesAndBraceletsXPath);
         WebElement linkWatchesAndBraceletsWebElement = driver.findElement(linkWatchesAndBraceletsBy);
         String expectedLinkWatchesAndBracelets = linkWatchesAndBraceletsWebElement.getText();
