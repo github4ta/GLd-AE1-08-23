aa7c08e1d1b86cafa1789ab3bb9923103a392cdc
serge <sergei-msg@tut.by>
2024-08-08 21:51:59 +0300

    add testHPFT007() to check "public agreement"

--- src/test/java/by/onliner/homepage/FooterTest.java
+++ src/test/java/by/onliner/homepage/FooterTest.java
@@ -1,8 +1,13 @@
 package by.onliner.homepage;
 
+import by.onliner.Waiters;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FooterTest {
@@ -15,6 +20,20 @@ public class FooterTest {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(BASE_URL);
+    }
+
+    @Test
+    public void testHPFT007() {
+        Waiters.waitFor(3);
+        String xPathPublicAgreement = "//a[@href='https://blog.onliner.by/publichnye-dogovory']";
+        By byFooterPublicAgreement = By.xpath(xPathPublicAgreement);
+        WebElement webElementXpathPublicAgreement = driver.findElement(byFooterPublicAgreement);
+
+        String expectedTextPublicAgreement = "Публичные договоры";
+        String actualResultPublicAgreement = webElementXpathPublicAgreement.getText();
+
+        Assertions.assertEquals(expectedTextPublicAgreement, actualResultPublicAgreement);
+
     }
 
     @AfterEach
