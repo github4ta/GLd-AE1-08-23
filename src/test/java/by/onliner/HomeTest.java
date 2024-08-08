797bca0370ee42c8f0dafde5b0f68382b7bfbda8
Sergei Tsarik <serge.tsarik@gmail.com>
2024-08-08 17:55:49 +0300

    add home page test class

---
+++ src/test/java/by/onliner/HomeTest.java
@@ -0,0 +1,24 @@
+package by.onliner;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomeTest {
+
+    private WebDriver driver;
+    private final String BASE_URL = "https://www.onliner.by/";
+
+    @BeforeEach
+    public void beforeEach() {
+        driver = new ChromeDriver();
+        driver.get(BASE_URL);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        driver.quit();
+    }
+
+}
