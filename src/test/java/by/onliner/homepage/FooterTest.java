a9a73e16fbae42d39aba97a09c5cb1b938e264fb
Dzmitryi Miadzvedzeu <dzmitryi.miadzvedzeu@gmail.com>
2024-08-09 21:38:57 +0300

    add UI Test Onliner.by пользовательское соглашение

--- src/test/java/by/onliner/homepage/FooterTest.java
+++ src/test/java/by/onliner/homepage/FooterTest.java
@@ -1,8 +1,12 @@
 package by.onliner.homepage;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FooterTest {
