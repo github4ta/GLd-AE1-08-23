df009b7028b64e6622b6b22c7fcf0b85256b8dd9
ZinkoAnton <antonzinko87@gmail.com>
2024-08-10 10:24:44 +0300

    add Anton Zinko OnlinerTest

--- src/test/java/by/onliner/OnlinerTest.java
+++ src/test/java/by/onliner/OnlinerTest.java
@@ -25,6 +25,105 @@ public class OnlinerTest {
     }
 
     @Test
+    public void testSSD() {
+        driver.manage().window().maximize();
+        String footerXpath = "//a[@href='https://catalog.onliner.by/ssd']/span/span";
+        By footerBy = By.xpath(footerXpath);
+        WebElement footerWebElement = driver.findElement(footerBy);
+
+        String actualText = footerWebElement.getText();
+        String expectedTextMessage = "SSD";
+
+        Assertions.assertEquals(expectedTextMessage, actualText);
+    }
+
+    @Test
+    public void test() {
+        String videoXpath = "//a[@href=\"https://catalog.onliner.by/videocard\"]";
+        By videoXpathBy = By.xpath(videoXpath);
+        WebElement videoXpathWebElement = driver.findElement(videoXpathBy);
+
+
+        // driver.findElement();
+
+        String actualVideoOne = videoXpathWebElement.getText();
+        String expectedVideoOne = "Видеокарты";
+        Assertions.assertEquals(expectedVideoOne, actualVideoOne);
+    }
+
+    @Test
+    public void testProcessors() {
+        driver.manage().window().maximize();
+        String fieldSiteEndTextXPath = "//a[@href='https://catalog.onliner.by/cpu']/span/span";
+        By fieldSiteEndTextBy = By.xpath(fieldSiteEndTextXPath);
+        WebElement fieldSiteEndTextWebElement = driver.findElement(fieldSiteEndTextBy);
+
+        String actualTextMessage = fieldSiteEndTextWebElement.getText();
+        String expectedTextMessage = "Процессоры";
+
+        Assertions.assertEquals(expectedTextMessage, actualTextMessage);
+    }
+
+    @Test
+    public void testMonitorsLink() {
+        driver.manage().window().maximize();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        String xPathMonitorsLink = "//a[@href='https://catalog.onliner.by/display']//span[contains(@class, 'project-navigation__sign')]";
+        By byMonitors = By.xpath(xPathMonitorsLink);
+
+        WebElement webElementMonitors = wait.until(ExpectedConditions.visibilityOfElementLocated(byMonitors));
+        String actualText = webElementMonitors.getText();
+        String expectedText = "Мониторы";
+        Assertions.assertEquals(expectedText, actualText);
+    }
+
+    @Test
+    public void testAirphoneLink() {
+        driver.manage().window().maximize();
+        String xPathAiphonesLink = "//a[@href=\"https://catalog.onliner.by/headphones\"] [@class=\"project-navigation__link project-navigation__link_primary\"]";
+        By byAirphones = By.xpath(xPathAiphonesLink);
+        WebElement webElementAirphone = driver.findElement(byAirphones);
+        String actualTest = webElementAirphone.getText();
+        String expectedTest = "Наушники и гарнитуры";
+        Assertions.assertEquals(expectedTest, actualTest);
+    }
+
+    @Test
+    public void testButtonNotebook() {
+        String buttonNotebookXPath = "//a[@href='https://catalog.onliner.by/notebook']//span[@class='project-navigation__sign']";
+        By buttonNotebookBy = By.xpath(buttonNotebookXPath);
+        WebElement buttonNotebookWebElement = driver.findElement(buttonNotebookBy);
+
+        String expectedButtonText = "Ноутбуки";
+        String actualButtonText = buttonNotebookWebElement.getText();
+
+        Assertions.assertEquals(expectedButtonText, actualButtonText);
+    }
+
+    @Test
+    public void testCleanersLink() {
+        driver.manage().window().maximize();
+        String xPathCleanerLink = "//a[@href='https://catalog.onliner.by/vacuumcleaner']/span/span";
+        By byCleaners = By.xpath(xPathCleanerLink);
+        WebElement webElementCleaners = driver.findElement(byCleaners);
+        String actualText = webElementCleaners.getText();
+        String expetedText = "Пылесосы";
+        Assertions.assertEquals(expetedText, actualText);
+    }
+
+    @Test
+    public void testUIButtonTVs() {
+        String fieldButtonTVsXPath = "//a[@href='https://catalog.onliner.by/tv']/span/span";
+        WebElement buttonTVsWebElement = driver.findElement(By.xpath(fieldButtonTVsXPath));
+        String actualText = buttonTVsWebElement.getText();
+        String expectedText = "Телевизоры";
+
+        Assertions.assertEquals(expectedText, actualText);
+
+    }
+
+    @Test
     public void testWatchesAndBracelets() {
         String linkWatchesAndBraceletsXPath = "//*[@id=\"container\"]/div/div/div/div/div[1]/div/div[1]/ul[2]/li[6]/a/span/span";
         By linkWatchesAndBraceletsBy = By.xpath(linkWatchesAndBraceletsXPath);
