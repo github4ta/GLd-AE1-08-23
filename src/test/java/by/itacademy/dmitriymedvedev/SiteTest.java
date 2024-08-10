1ae7254515696ed3c3740b130fe18fd1a92bc6c9
Dzmitryi Miadzvedzeu <dzmitryi.miadzvedzeu@gmail.com>
2024-08-10 22:16:37 +0300

    add test1 for https://taplink-irinachekanova.ru to SiteTest class

--- src/test/java/by/itacademy/dmitriymedvedev/SiteTest.java
+++ src/test/java/by/itacademy/dmitriymedvedev/SiteTest.java
@@ -1,5 +1,36 @@
 package by.itacademy.dmitriymedvedev;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SiteTest {
 
+    WebDriver driver;
+
+    @BeforeEach
+    public void beforeEach() {
+        driver = new ChromeDriver();
+        driver.get("https://taplink-irinachekanova.ru/");
+    }
+
+    @AfterEach
+    public void afterEach() {
+        driver.quit();
+    }
+
+    @Test
+    public void test1() {
+
+        String fieldWithEmailXPath = "//a[@href=\"mailto:irina.chekanova.astrolog@gmail.com\"]";
+        By fieldWithEmailBy = By.xpath(fieldWithEmailXPath);
+        WebElement fieldWithEmailByWebElement = driver.findElement(fieldWithEmailBy);
+
+        String actual = fieldWithEmailByWebElement.getText();
+        Assertions.assertEquals("irina.chekanova.astrolog@gmail.com",actual);
+    }
 }
