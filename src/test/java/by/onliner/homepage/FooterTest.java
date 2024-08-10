3e91e6b79ea3f578fe07d19e1f4e2daab1bf384b
serge <sergei-msg@tut.by>
2024-08-10 10:48:00 +0300

    fixed WebElement and used new XPath variant in testHPFT006()

--- src/test/java/by/onliner/homepage/FooterTest.java
+++ src/test/java/by/onliner/homepage/FooterTest.java
@@ -25,12 +25,13 @@ public class FooterTest {
     @Test
     public void testHPFT006() {
         Waiters.waitFor(3);
-        String xPathPublicAgreement = "//a[@href='https://blog.onliner.by/publichnye-dogovory']";
+        //String xPathPublicAgreement = "//a[@href='https://blog.onliner.by/publichnye-dogovory']";
+        String xPathPublicAgreement = "//a[contains(text(), 'Публичные договоры')]";
         By byFooterPublicAgreement = By.xpath(xPathPublicAgreement);
-        WebElement webElementXpathPublicAgreement = driver.findElement(byFooterPublicAgreement);
+        WebElement webElementPublicAgreement = driver.findElement(byFooterPublicAgreement);
 
         String expectedTextPublicAgreement = "Публичные договоры";
-        String actualResultPublicAgreement = webElementXpathPublicAgreement.getText();
+        String actualResultPublicAgreement = webElementPublicAgreement.getText();
 
         Assertions.assertEquals(expectedTextPublicAgreement, actualResultPublicAgreement);
 
