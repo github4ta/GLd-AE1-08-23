a3dea1e4309a5faed55dd0a2483f01868c39d5ec
Aksana Bazhenava <aksana.bazhenava@gmail.com>
2024-08-11 18:28:10 +0300

    Задание Footer FT008 (#66)
    Co-authored-by: Aksana Bazhenava <bazhenava1.17@gmail.com>

---
+++ src/test/java/by/onliner/footerpages/FT008Test.java
@@ -0,0 +1,46 @@
+package by.onliner.footerpages;
+
+import by.onliner.Waiters;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FT008Test {
+
+    private WebDriver driver;
+    private final String BASE_URL = "https://www.onliner.by/";
+
+    @BeforeEach
+    public void beforeEach() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(BASE_URL);
+    }
+
+    @Test
+    public void testWhenClickButtonUserSupportThenUserSupportOpened() {
+        Waiters.waitFor(2);
+        String buttonUserSupportXpath = "//a[@href='https://support.onliner.by']";
+        By buttonUserSupportBy = By.xpath(buttonUserSupportXpath);
+        WebElement buttonUserSupportWebElement = driver.findElement(buttonUserSupportBy);
+        buttonUserSupportWebElement.click();
+        Waiters.waitFor(2);
+        String userSupportXpath = "//div[@id='main']/div/h1";
+        By userSupportBy = By.xpath(userSupportXpath);
+        WebElement userSupportWebElement = driver.findElement(userSupportBy);
+        String actualText = userSupportWebElement.getText();
+        String expectedText = "Запрос в службу поддержки";
+
+        Assertions.assertEquals(expectedText, actualText);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        driver.quit();
+    }
+}
