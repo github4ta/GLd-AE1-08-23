3d74a94e2f75875dc328a4ff32444363dec6ad42
lu55y <mu11er100500@gmail.com>
2024-08-11 14:49:20 +0300

    add setupDriver() and teardown() working methods

--- src/test/java/by/onliner/footerpages/FT006Test.java
+++ src/test/java/by/onliner/footerpages/FT006Test.java
@@ -1,4 +1,24 @@
 package by.onliner.footerpages;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FT006Test {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setupDriver() {
+        String workingPageURL = "https://www.onliner.by/";
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(workingPageURL);
+    }
+
+
+    @AfterEach
+    public void teardown() {
+        driver.quit();
+    }
 }
