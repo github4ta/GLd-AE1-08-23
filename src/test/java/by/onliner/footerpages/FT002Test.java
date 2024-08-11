09ea4914834a2dcd8efbb8088843add63ef4d1ab
serge <sergei-msg@tut.by>
2024-08-11 16:48:30 +0300

    add test to check title header text Editor Contacts page class FT002TEst

--- src/test/java/by/onliner/footerpages/FT002Test.java
+++ src/test/java/by/onliner/footerpages/FT002Test.java
@@ -28,14 +28,26 @@ public class FT002Test {
         By xPathlinkEditorContacts = By.xpath(linkEditorContacts);
         WebElement webElementEditorContacts = driver.findElement(xPathlinkEditorContacts);
         webElementEditorContacts.click();
+
         String expectedEditorContactLink = "https://people.onliner.by/contacts";
         String actualEditorContactsLink = driver.getCurrentUrl();
         Assertions.assertEquals(expectedEditorContactLink, actualEditorContactsLink);
+
+        String titleEditorContacts = "//div[@class='news-header__title']";
+        By xPathTitleEditorContact = By.xpath(titleEditorContacts);
+        WebElement webElementTitleEditorContacts = driver.findElement(xPathTitleEditorContact);
+
+        String expectedTitleEditorContacts =
+                "Контакты редакции. Манифест. Спецпроекты. Правила использования материалов";
+        String actualTitleEditorContact = webElementTitleEditorContacts.getText();
+        Assertions.assertEquals(expectedTitleEditorContacts, actualTitleEditorContact);
+
+
+
     }
 
     @AfterEach
     public void afterEach(){
         driver.quit();
     }
-
 }
