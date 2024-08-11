6fffa5cc65ae35e7ee73842b2d1493e9f38b564a
lu55y <mu11er100500@gmail.com>
2024-08-11 14:50:43 +0300

    add and implement testWhenUserClickPublicAgreementsThenPublicAgreementsPageOpened() method

--- src/test/java/by/onliner/footerpages/FT006Test.java
+++ src/test/java/by/onliner/footerpages/FT006Test.java
@@ -1,8 +1,12 @@
 package by.onliner.footerpages;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FT006Test {
@@ -16,6 +20,19 @@ public class FT006Test {
         driver.get(workingPageURL);
     }
 
+    @Test
+    public void testWhenUserClickPublicAgreementsThenPublicAgreementsPageOpened() {
+        String referencePublicAgreementsXPath = "//a[@href='https://blog.onliner.by/publichnye-dogovory']";
+        WebElement referencePublicAgreementsWebElement = driver.findElement(By.xpath(referencePublicAgreementsXPath));
+        referencePublicAgreementsWebElement.click();
+
+        String titleFieldPublicAgreementsPageXPath = "//div[@class='news-header__title']";
+        WebElement titleFieldPublicAgreementsPageWebElement = driver.findElement(By.xpath(titleFieldPublicAgreementsPageXPath));
+        String actualFieldText = titleFieldPublicAgreementsPageWebElement.getText();
+        String exceptedFieldText = "Публичные договоры";
+
+        Assertions.assertEquals(exceptedFieldText, actualFieldText);
+    }
 
     @AfterEach
     public void teardown() {
