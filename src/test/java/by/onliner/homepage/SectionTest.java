a5552f4a9bbe3b82301d84f2316f712e4770782c
Andrei Unuchak <andrei.unuchak@gmail.com>
2024-08-13 11:10:42 +0200

    [ADD] Added testSectionBaraholka method

--- src/test/java/by/onliner/homepage/SectionTest.java
+++ src/test/java/by/onliner/homepage/SectionTest.java
@@ -1,6 +1,28 @@
 package by.onliner.homepage;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
 public class SectionTest extends BaseTest {
 
+    @Test
+    public void testSectionBaraholka(){
+        String topMenuXpath = "//ul[@class='b-main-navigation']";
+        By topMenuBy = By.xpath(topMenuXpath);
+        WebElement topMenuWebElement = driver.findElement(topMenuBy);
+        String menuOptionXpath = "//li[contains(@class,'b-main-navigation__item')]";
+        By menuOptionBy = By.xpath(menuOptionXpath);
+        List<WebElement> menuOptionWebElements = topMenuWebElement.findElements(menuOptionBy);
+        WebElement buttonBaraholka = menuOptionWebElements.get(5);
+        buttonBaraholka.click();
 
+        String expectedBaraholkaURL = "https://baraholka.onliner.by/";
+        String actualBaraholkaURL = driver.getCurrentUrl();
+
+        Assertions.assertEquals(expectedBaraholkaURL, actualBaraholkaURL);
+    }
 }
