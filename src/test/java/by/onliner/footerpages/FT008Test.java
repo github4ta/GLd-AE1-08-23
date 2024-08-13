54c5e36a2c96300b70affcb10640fd265cfa6151
Aksana Bazhenava <bazhenava1.17@gmail.com>
2024-08-13 14:35:55 +0300

    refactor class FT008Test

--- src/test/java/by/onliner/footerpages/FT008Test.java
+++ src/test/java/by/onliner/footerpages/FT008Test.java
@@ -9,12 +9,12 @@ import org.openqa.selenium.WebElement;
 public class FT008Test extends BaseTest {
 
     @Test
-    public void testWhenClickButtonUserSupportThenUserSupportOpened() {
+    public void testWhenClickOnLinkUserSupportThenUserSupportOpened() {
         Waiters.waitFor(2);
-        String buttonUserSupportXpath = "//a[@href='https://support.onliner.by']";
-        By buttonUserSupportBy = By.xpath(buttonUserSupportXpath);
-        WebElement buttonUserSupportWebElement = driver.findElement(buttonUserSupportBy);
-        buttonUserSupportWebElement.click();
+        String linkUserSupportXpath = "//a[@href='https://support.onliner.by']";
+        By linkUserSupportBy = By.xpath(linkUserSupportXpath);
+        WebElement linkUserSupportWebElement = driver.findElement(linkUserSupportBy);
+        linkUserSupportWebElement.click();
         Waiters.waitFor(2);
         String userSupportXpath = "//div[@id='main']/div/h1";
         By userSupportBy = By.xpath(userSupportXpath);
