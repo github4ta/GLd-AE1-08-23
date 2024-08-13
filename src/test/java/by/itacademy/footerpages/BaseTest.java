20c07beb488aa8be15416d89efb3bed1ee5a2c64
ZinkoAnton <antonzinko87@gmail.com>
2024-08-13 14:22:07 +0300

    Add Anton Zinko BaseTest

---
+++ src/test/java/by/itacademy/footerpages/BaseTest.java
@@ -0,0 +1,23 @@
+package by.itacademy.footerpages;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+    WebDriver driver;
+    private final String BASE_URL = "https://www.onliner.by/";
+
+    @BeforeEach
+    public void beforeEach() {
+        driver = new ChromeDriver();
+        driver.get(BASE_URL);
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        driver.quit();
+    }
+}
--- src/test/java/by/itacademy/footerpages/FT009Test.java
+++ src/test/java/by/itacademy/footerpages/FT009Test.java
@@ -1,24 +1,11 @@
 package by.itacademy.footerpages;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class FT009Test {
-    private WebDriver driver;
-    private final String BASE_URL = "https://www.onliner.by/";
-
-    @BeforeEach
-    public void beforeEach() {
-        driver = new ChromeDriver();
-        driver.get(BASE_URL);
-        driver.manage().window().maximize();
-    }
+public class FT009Test extends BaseTest {
 
     @Test
     public void testWhenUserClickReturnPolicyThenRulesForReturningGoodsAndMoney() {
@@ -35,9 +22,5 @@ public class FT009Test {
         String actualPageRulesForReturningGoodsAndMoney = "Правила возврата товаров и денег";
         Assertions.assertEquals(expectedPageRulesForReturningGoodsAndMoney, actualPageRulesForReturningGoodsAndMoney);
     }
-
-    @AfterEach
-    public void afterEach() {
-        driver.quit();
-    }
+    
 }
