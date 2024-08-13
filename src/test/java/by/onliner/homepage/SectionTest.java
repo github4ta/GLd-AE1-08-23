ca6159864715c354dae4dc71ceb10d4a20bcdd85
Aksana Bazhenava <bazhenava1.17@gmail.com>
2024-08-13 14:20:29 +0300

    refactor class SectionTest

--- src/test/java/by/onliner/homepage/SectionTest.java
+++ src/test/java/by/onliner/homepage/SectionTest.java
@@ -1,6 +1,28 @@
 package by.onliner.homepage;
 
+import by.onliner.Waiters;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 public class SectionTest extends BaseTest {
 
+    @Test
+    public void testWhenClickButtonForumThenForumOpened() {
+        Waiters.waitFor(1);
+        String buttonForumXpath = "//*[@id='container']/div/div/div/div/div[18]/header/h2/a";
+        By buttonForumBy = By.xpath(buttonForumXpath);
+        WebElement buttonForumWebElement = driver.findElement(buttonForumBy);
+        buttonForumWebElement.click();
 
+        Waiters.waitFor(1);
+        String forumXpath = "//*[@id=\"minWidth\"]/div/div[2]/div[2]/div/div[2]/h1";
+        By forumBy = By.xpath(forumXpath);
+        WebElement forumWebElement = driver.findElement(forumBy);
+        String actualText = forumWebElement.getText();
+        String expectedText = "Форум";
+
+        Assertions.assertEquals(expectedText, actualText);
+    }
 }
