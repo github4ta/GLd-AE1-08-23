f7dbe2a9194af7b87fd4f9a566b04745150fe6b9
Andrei Unuchak <andrei.unuchak@gmail.com>
2024-08-13 11:53:29 +0200

    [REFACTOR] Removed empty line

--- src/test/java/by/onliner/homepage/SectionTest.java
+++ src/test/java/by/onliner/homepage/SectionTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-
 import java.util.List;
 
 public class SectionTest extends BaseTest {
