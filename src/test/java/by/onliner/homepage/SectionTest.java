bd3f4bd346fb4c4514470eb8333ba7c6e73bf85b
Aksana Bazhenava <bazhenava1.17@gmail.com>
2024-08-14 10:03:23 +0300

    refactor class SectionTest

--- src/test/java/by/onliner/homepage/SectionTest.java
+++ src/test/java/by/onliner/homepage/SectionTest.java
@@ -17,7 +17,7 @@ public class SectionTest extends BaseTest {
         buttonForumWebElement.click();
 
         Waiters.waitFor(1);
-        String forumXpath = "//*[@id=\'minWidth\']/div/div[2]/div[2]/div/div[2]/h1";
+        String forumXpath = "//*[@id='minWidth']/div/div[2]/div[2]/div/div[2]/h1";
         By forumBy = By.xpath(forumXpath);
         WebElement forumWebElement = driver.findElement(forumBy);
         String actualForumText = forumWebElement.getText();
