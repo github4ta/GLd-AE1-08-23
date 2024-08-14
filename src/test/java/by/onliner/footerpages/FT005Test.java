2b9622dd5fe928dac02e37f9b6f1d53c44184e7a
andreiunuchak <43380444+andreiunuchak@users.noreply.github.com>
2024-08-14 18:03:13 +0200

    Refactor FT005 (#81)

--- src/test/java/by/onliner/footerpages/FT005Test.java
+++ src/test/java/by/onliner/footerpages/FT005Test.java
@@ -1,27 +1,12 @@
 package by.onliner.footerpages;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
-public class FT005Test {
-    private WebDriver driver;
+public class FT005Test extends BaseTest{
     private final String BASE_URL = "https://www.onliner.by/";
-
-    @BeforeEach
-    public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-search-engine-choice-screen");
-        driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
-        driver.get(BASE_URL);
-    }
 
     @Test
     public void testWhenUserClickSiteRulesThenSiteRulesOpened() {
@@ -41,10 +26,5 @@ public class FT005Test {
         String expectedTitleSiteRulesText = "Пользовательское соглашение/Правила";
         String actualTitleSiteRulesText = titleSiteRulesTextWebElement.getText();
         Assertions.assertEquals(expectedTitleSiteRulesText, actualTitleSiteRulesText);
-    }
-
-    @AfterEach
-    public void tearDown() {
-        driver.quit();
     }
 }
