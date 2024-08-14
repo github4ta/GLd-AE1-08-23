a02d9e2e0dcfafeff4cb92ef81db93e1247411c6
Aksana Bazhenava <bazhenava1.17@gmail.com>
2024-08-14 14:52:33 +0300

    refactor classes BaseTest and FT008Test

--- src/test/java/by/onliner/footerpages/FT008Test.java
+++ src/test/java/by/onliner/footerpages/FT008Test.java
@@ -15,6 +15,7 @@ public class FT008Test extends BaseTest {
         By linkUserSupportBy = By.xpath(linkUserSupportXpath);
         WebElement linkUserSupportWebElement = driver.findElement(linkUserSupportBy);
         linkUserSupportWebElement.click();
+
         Waiters.waitFor(2);
         String userSupportXpath = "//div[@id='main']/div/h1";
         By userSupportBy = By.xpath(userSupportXpath);
