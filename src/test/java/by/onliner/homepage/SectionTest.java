ad35d8335b9f53ccdcf1f5cae9b0fc8d0d529b4f
andreiunuchak <43380444+andreiunuchak@users.noreply.github.com>
2024-08-21 10:53:56 +0200

    Task ST004 (#82)
    Co-authored-by: ArtjomMironchyk <artjommironchyk@gmail.com>
    Co-authored-by: ZinkoAnton <antonzinko87@gmail.com>
    Co-authored-by: lu55y <mu11er100500@gmail.com>
    Co-authored-by: Aksana Bazhenava <aksana.bazhenava@gmail.com>
    Co-authored-by: Aksana Bazhenava <bazhenava1.17@gmail.com>
    Co-authored-by: Dzmitryi Miadzvedzeu <dzmitryi.miadzvedzeu@gmail.com>
    Co-authored-by: sergeimsg <98679437+sergeimsg@users.noreply.github.com>
    Co-authored-by: Pavel Yatsevich <71383625+pavel-yatsevich@users.noreply.github.com>
    Co-authored-by: IrinaNovik00 <novikirishka7@gmail.com>
    Co-authored-by: Olga <128300171+zaitsevaolv@users.noreply.github.com>
    Co-authored-by: Olga.Zaitseva <olga.zaitseva.itacademy@gmail.com>
    Co-authored-by: Denis-Baranov-1996 <denizzka96@gmail.com>
    Co-authored-by: Sergei Tsarik <serge.tsarik@gmail.com>

--- src/test/java/by/onliner/homepage/SectionTest.java
+++ src/test/java/by/onliner/homepage/SectionTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import java.util.List;
 
 public class SectionTest extends HomeBaseTest {
 
@@ -24,5 +25,22 @@ public class SectionTest extends HomeBaseTest {
         String expectedForumText = "Форум";
 
         Assertions.assertEquals(expectedForumText, actualForumText);
+    }
+
+    @Test
+    public void testSectionBaraholka(){
+        String topMenuXpath = "//ul[@class='b-main-navigation']";
+        By topMenuBy = By.xpath(topMenuXpath);
+        WebElement topMenuWebElement = driver.findElement(topMenuBy);
+        String menuOptionXpath = "//li[contains(@class,'b-main-navigation__item')]";
+        By menuOptionBy = By.xpath(menuOptionXpath);
+        List<WebElement> menuOptionWebElements = topMenuWebElement.findElements(menuOptionBy);
+        WebElement buttonBaraholkaWebElement = menuOptionWebElements.get(5);
+        buttonBaraholkaWebElement.click();
+
+        String expectedBaraholkaURL = "https://baraholka.onliner.by/";
+        String actualBaraholkaURL = driver.getCurrentUrl();
+
+        Assertions.assertEquals(expectedBaraholkaURL, actualBaraholkaURL);
     }
 }
