16115e6301675c1ededb5f6e05f3b3047a2f5b2a
Dzmitryi Miadzvedzeu <dzmitryi.miadzvedzeu@gmail.com>
2024-08-25 14:55:20 +0300

    add addMargaritaPizzaToCart, addToCart, getTitleInCart methods to MenuPage, add String IMAGE_TITLE for search strategy, code refactoring in PapaJohnsPizzaTest

--- src/test/java/by/itacademy/dmitriymedvedev/PapaJohnsPizzaTest.java
+++ src/test/java/by/itacademy/dmitriymedvedev/PapaJohnsPizzaTest.java
@@ -10,9 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-
 public class PapaJohnsPizzaTest extends BasePapaJohnsTest {
-
     @BeforeEach
     public void closePopup() throws InterruptedException {
         By buttonCloseBy = By.xpath(CityChooseModalPage.BUTTON_CLOSE_XPATH);
@@ -20,35 +18,25 @@ public class PapaJohnsPizzaTest extends BasePapaJohnsTest {
         Thread.sleep(5000);
         buttonCloseWebElement.click();
     }
+
     @Test
     public void openMenuPizza() throws InterruptedException {
         By menuButtonPizzaBy = By.xpath(MenuPage.BUTTON_PIZZA);
         WebElement menuButtonPizzaWebElement = driver.findElement(menuButtonPizzaBy);
         menuButtonPizzaWebElement.click();
-        //Thread.sleep(5000);
 
-
-        WebElement image = driver.findElement(By.xpath("//span[text()='Маргарита']"));
+        WebElement image = driver.findElement(By.xpath(MenuPage.IMAGE_TITLE));
         new Actions(driver)
                 .scrollToElement(image)
                 .perform();
 
+        MenuPage menuPage;
+        menuPage = new MenuPage(driver);
+        menuPage.addMargaritaPizzaToCart();
+        menuPage.addToCart();
 
-        By addMargaritaPizzaBy = By.xpath(MenuPage.ADD_MARGARITA_PIZZA);
-        WebElement addMargaritaPizzWebElement = driver.findElement(addMargaritaPizzaBy);
-        addMargaritaPizzWebElement.click();
-
-        By addToCartBy = By.xpath(MenuPage.CLICK_ADD_TO_CART);
-        WebElement addToCartByPizzWebElement = driver.findElement(addToCartBy);
-        addToCartByPizzWebElement.click();
-
-
-        By titleCartBy = By.xpath(MenuPage.TITLE_IN_CART);
-        WebElement titleCartWebElement = driver.findElement(titleCartBy);
-
-        String actualTitlePizzaText = titleCartWebElement.getText();
+        String actualTitlePizzaText = menuPage.getTitleInCart();
         String expectedTitlePizzaText = "Маргарита";
         Assertions.assertEquals(expectedTitlePizzaText, actualTitlePizzaText);
-        Thread.sleep(10000);
     }
 }
