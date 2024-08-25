5b0fb0d9f27ced28328f7ebcdeb17d0f90103f6e
ZinkoAnton <antonzinko87@gmail.com>
2024-08-25 15:19:42 +0300

    Update DodopizzaPizzaPage.java

--- src/main/java/by/itacademy/antonzinko/pages/DodopizzaPizzaPage.java
+++ src/main/java/by/itacademy/antonzinko/pages/DodopizzaPizzaPage.java
@@ -3,13 +3,13 @@ package by.itacademy.antonzinko.pages;
 import org.openqa.selenium.By;
 
 public class DodopizzaPizzaPage extends BasePage {
-    public DodopizzaPizzaPage() {
-        super();
-    }
-
     private final String MENU_BUTTON_PIZZA_XPATH = "//*[@id=\"rdgoc\"]/article[31]/main/div/a";
     private final By menuButtonPizzaBy = By.xpath(MENU_BUTTON_PIZZA_XPATH);
 
+    public DodopizzaPizzaPage() {
+        super();
+    }
+    
     public void openPage() {
         final String BASE_URL = "https://dodopizza.by/minsk";
         driver.navigate().to(BASE_URL);
