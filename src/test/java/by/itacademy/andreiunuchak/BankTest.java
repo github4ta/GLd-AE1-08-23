754efffc80e1cddc6fde7490700efd6a79bfcdbf
Andrei Unuchak <andrei.unuchak@gmail.com>
2024-09-03 12:30:41 +0200

    [ADD] Jackson dependency [ADD] Tests to get list of Cur_ID from response with Gson and Jackson

--- src/test/java/by/itacademy/andreiunuchak/BankTest.java
+++ src/test/java/by/itacademy/andreiunuchak/BankTest.java
@@ -1,12 +1,18 @@
 package by.itacademy.andreiunuchak;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import static io.restassured.RestAssured.when;
 
@@ -36,5 +42,45 @@ public class BankTest {
                 .collect(Collectors.toList());
 
         System.out.println(curId);
+    }
+
+    @Test
+    public void testCurrenciesWithGsonExample(){
+        Response response = when().get("https://api.nbrb.by/exrates/currencies");
+        String body = response.body().asString();
+
+        JsonParser jsonParser = new JsonParser();
+        JsonArray jsonArray = jsonParser.parse(body).getAsJsonArray();
+
+        List<Integer> curIds = new ArrayList<>();
+        for (JsonElement element : jsonArray) {
+            JsonElement curIdElement = element.getAsJsonObject().get("Cur_ID");
+            if (curIdElement != null && curIdElement.isJsonPrimitive()) {
+                curIds.add(curIdElement.getAsInt());
+            }
+        }
+
+        System.out.println(curIds);
+    }
+
+    @Test
+    public void testCurrennciesWithJackson(){
+        Response response = when().get("https://api.nbrb.by/exrates/currencies");
+        String jsonString = response.body().asString();
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Integer> curIds = new ArrayList<>();
+
+        try {
+            JsonNode rootNode = objectMapper.readTree(jsonString);
+            if (rootNode.isArray()) {
+                for (JsonNode node : rootNode) {
+                    int curId = node.path("Cur_ID").asInt();
+                    curIds.add(curId);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(curIds);
     }
 }
