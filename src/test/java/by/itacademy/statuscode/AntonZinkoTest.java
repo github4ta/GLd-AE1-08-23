71311fbcd585ca648aabca8e537c74823d0df1ae
ZinkoAnton <antonzinko87@gmail.com>
2024-09-04 19:24:31 +0300

    Zinkoanton api Test (#114)
    Задание выполнено

---
+++ src/test/java/by/itacademy/statuscode/AntonZinkoTest.java
@@ -0,0 +1,49 @@
+package by.itacademy.statuscode;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class AntonZinkoTest {
+
+    @Test
+    public void testDemoqa() {
+        given().
+                when().
+                get("https://demoqa.com/BookStore/v1/Books").
+                then().
+                statusCode(200).
+                body("books.title", hasItem(containsString("Git")));
+    }
+
+    @Test
+    public void testApiOceandrivers() {
+        given().
+                when().
+                get("http://api.oceandrivers.com/v1.0/getWebCams/").
+                then().
+                statusCode(200).
+                body("name", hasItem(containsString("Cala Agulla")));
+    }
+
+    @Test
+    public void testApiHandwrytten() {
+        given().
+                when().
+                get("https://api.handwrytten.com/v1/cards/list").
+                then().
+                statusCode(200).
+                body("cards.name", hasItem(containsString("Flat 5x7 Card")));
+    }
+
+    @Test
+    public void testRestfulBookerHerokuapp() {
+        given().
+                when().
+                get("https://restful-booker.herokuapp.com/booking").
+                then().
+                statusCode(200).
+                body("bookingid", hasItem(168));
+    }
+}
