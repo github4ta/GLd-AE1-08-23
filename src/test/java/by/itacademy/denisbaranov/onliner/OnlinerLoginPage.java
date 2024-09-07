9aa630b450166a36bc87c98ef33006bfd0d487f3
Denis-Baranov-1996 <denizzka96@gmail.com>
2024-09-08 00:53:48 +0300

    add OnlinerLoginPage java add OnlinerTest java

--- src/test/java/by/itacademy/denisbaranov/onliner/OnlinerLoginPage.java
+++ src/test/java/by/itacademy/denisbaranov/onliner/OnlinerLoginPage.java
@@ -1,27 +1,29 @@
 package by.itacademy.denisbaranov.onliner;
 
+import io.restassured.response.Response;
+
 import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
 public class OnlinerLoginPage {
+
+
+
     public static final String SERVICE_LOGIN = "/login";
 
     public static String getBody() {
-        String body = "{\n" +
-                "    \"login\": \"123@yandex.by\",\n" +
-                "    \"password\": \"123456\"\n" +
-                "}";
-        return body;
+        return getBody("qweqwe", "123");
     }
 
     public static String getBody(String login, String password) {
         String body = "{\n" +
-                "    \"login\": \"123@yandex.by\",\n" +
-                "    \"password\": \"123456\"\n" +
+                "    \"login\": \"" + login + "\",\n" +
+                "    \"password\": \"" + password + "\"\n" +
                 "}";
         String body2 = String.format("{\"login\":\"%s\",\"password\":\"%s\"}", login, password);
+        //String body3 = ...;
         return body;
     }
 
--- src/test/java/by/itacademy/denisbaranov/onliner/OnlinerTest.java
+++ src/test/java/by/itacademy/denisbaranov/onliner/OnlinerTest.java
@@ -1,5 +1,7 @@
 package by.itacademy.denisbaranov.onliner;
 
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -15,4 +17,5 @@ public class OnlinerTest extends OnlinerBaseTest {
                 post(OnlinerLoginPage.SERVICE_LOGIN).
         then().log().all();
     }
+
 }
